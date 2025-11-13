d14881835fe02a1770b7859d1deb1d92172fd894
Marcel <jsoservice@yahoo.com>
2025-11-12 23:59:24 -0500

    Add files via upload

--- Formula/mgdiff.rb
+++ Formula/mgdiff.rb
@@ -5,12 +5,14 @@ class mgdiff < Formula
   sha256 "3fe48cfcf4d016c5409d2fb6c0664ceb05d6c62227d6af9b6884c8e801ac1ea9"
   license "MIT"
   revision 1
-    
+
   depends_on "openmotif"
-    
+
   def install
     bin.install "mgdiff"
     man1.install "mgdiff.man" => "mgdiff.1"
     (etc/"X11/app-defaults").install "Mgdiff.ad" => "Mgdiff"
-  end  
+  end
+
+  end
 end
