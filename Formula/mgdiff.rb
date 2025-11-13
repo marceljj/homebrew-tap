ad92ed28d1b5c0c6fd85de0e1402ae72949b4ac9
Marcel <jsoservice@yahoo.com>
2025-11-13 00:28:13 -0500

    Update mgdiff.rb

--- Formula/mgdiff.rb
+++ Formula/mgdiff.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://docs.brew.sh/rubydoc/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Mgdiff < Formula
   desc "A graphical frontend to the diff command, written using motif"
   homepage "https://x11cp.org/apps/mgdiff"
@@ -10,30 +7,13 @@ class Mgdiff < Formula
 
   depends_on "openmotif"
 
-  # Additional dependency
-  # resource "" do
-  #   url ""
-  #   sha256 ""
-  # end
-
   def install
-    # Remove unrecognized options if they cause configure to fail
-    # https://docs.brew.sh/rubydoc/Formula.html#std_configure_args-instance_method
     bin.install "mgdiff"
     man1.install "mgdiff.man" => "mgdiff.1"
     (etc/"X11/app-defaults").install "Mgdiff.ad" => "Mgdiff"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test mgdiff`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system bin/"program", "do", "something"`.
     system "false"
   end
 end
