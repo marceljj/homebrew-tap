2b42a171d677ed1c60d609b2b7531c664c908c55
Marcel <jsoservice@yahoo.com>
2025-11-13 23:56:44 -0500

    Update xinvest.rb

--- Formula/xinvest.rb
+++ Formula/xinvest.rb
@@ -1,16 +1,15 @@
 class Xinvest < Formula
   desc "A program which provides personal finance tracking and performance capabilities"
   homepage "https://x11cp.org/apps/xinvest"
-  url "https://marceljj.org/pkg/xinvest-2.5.1.tar.gz"
+  url "https://marceljj.org/pkg/bin/xinvest-2.5.1.tar.gz"
   version "2.5.1"
-  sha256 "91296f8c44ad8cc26c6bd0909fc027dd5737f2cb6a0be864ac06cb26e49874c7"
+  sha256 "2013d46d05739f48a0798a24b0e038e68940829deebdb720f26af7993d9381ef"
 
-  depends_on "make"
-  depends_on "imake"
+  depends_on arch: :arm64
+  depends_on :macos
   depends_on "openmotif"
 
   def install
-    system "xmkmf && ./build.sh"
     bin.install "Xinvest"
     (lib/"X11/app-defaults").install "Xinvest.ad" => "Xinvest"
   end
