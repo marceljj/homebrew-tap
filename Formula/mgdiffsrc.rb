218a25e32b1e5b423ae8606c55a4c9866b5a3f07
Marcel <jsoservice@yahoo.com>
2025-11-13 23:05:13 -0500

    Update mgdiffsrc.rb

--- Formula/mgdiffsrc.rb
+++ Formula/mgdiffsrc.rb
@@ -1,7 +1,7 @@
 class Mgdiffsrc < Formula
   desc "A graphical frontend to the diff command, written using Motif"
   homepage "https://x11cp.org/apps/mgdiff"
-  url "https://marceljj.org/pkg/mgdiffsrc-1.0.tgz"
+  url "https://marceljj.org/pkg/mgdiffsrc-1.0.tar.gz"
   version "1.0"
   sha256 "ab3db2cf618e3d914dbd3dad3ecc7cfe370fdd1970a9e13c938a4c37be794c86"
 
