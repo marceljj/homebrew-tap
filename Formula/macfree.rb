1f125a50b1d2371296bfa28b949a038c5d437e0c
Marcel <jsoservice@yahoo.com>
2025-11-19 03:12:32 -0500

    Update macfree.rb

--- Formula/macfree.rb
+++ Formula/macfree.rb
@@ -2,6 +2,7 @@ class Macfree< Formula
   desc "Disable Gatekeeper protection for a given macOS application"
   homepage "https://github.com/marceljj/macfree"
   url "https://github.com/marceljj/macfree.git", branch: "main"
+  license "GPL-3.0"
   version "0.1"
 
   depends_on :macos
