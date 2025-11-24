b7210ed4764d02900689d0b60e6feff1cb3bdb23
Marcel <jsoservice@yahoo.com>
2025-11-23 23:33:27 -0500

    Update xmtoolbar.rb

--- Formula/xmtoolbar.rb
+++ Formula/xmtoolbar.rb
@@ -3,8 +3,10 @@ class Xmtoolbar < Formula
   homepage "https://x11cp.org/apps/xmtoolbar"
   url "https://marceljj.org/pkg/xmtoolbar-1.2.tar.gz"
   version "1.2"
-  sha256 "6a9dfc23e5a774bdfabeafcff9a432aff3445ed6251a982624ffc27a38370375"
+  sha256 "2607ed97ffe640d775d098bad1d1bfe2673c09d8b4749ae7229aae3cbbdd25a9"
 
+  depends_on arch: :arm64
+  depends_on :macos
   depends_on "make"
   depends_on "openmotif"
 
