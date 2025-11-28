6235338b9356e2aee2cb24902366cf7249ac5db1
Marcel <jsoservice@yahoo.com>
2025-11-28 02:00:55 -0500

    Add files via upload

---
+++ Formula/udu.rb
@@ -0,0 +1,23 @@
+class Udu < Formula
+  desc "An extremely fast command-line disk usage analyzer"
+  homepage "https://github.com/gnualmalki/udu"
+  url "https://github.com/gnualmalki/udu.git", branch: "main"
+  license "GPL-3.0"
+  version "0.6.6"
+
+  depends_on "llvm
+  depends_on "libomp"
+  depends_on "make"
+
+  def install
+    system "gmake", "CC=clang", "-B"
+    bin.install "udu"
+    man1.install "udu.1"
+    (share/"doc/udu").install "COPYING"
+    (share/"doc/udu").install "README.md"
+  end
+
+  test do
+    system "false"
+  end
+end
