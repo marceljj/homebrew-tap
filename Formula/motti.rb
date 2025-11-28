fb9084c69994d783e21b92940184a0f7e4691517
Marcel <jsoservice@yahoo.com>
2025-11-28 01:39:12 -0500

    Update motti.rb

--- Formula/motti.rb
+++ Formula/motti.rb
@@ -9,7 +9,7 @@ class Motti < Formula
   depends_on "make"
 
   def install
-    system "./configure", *std_configure_args, "--bindir=#{bindir}", "--disable-silent-rules"
+    system "./configure", "--bindir=#{bindir}", "--prefix=#{prefix}", "--libdir=#{libdir}", "--disable-debug", "--disable-dependency-tracking", "--disable-silent-rules"
     system "make"
     system "make install"
   end
