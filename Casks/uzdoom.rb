320e4ad08a43506c96172d22fbf0493d15b77005
Marcel <jsoservice@yahoo.com>
2025-11-28 21:56:57 -0500

    Update uzdoom.rb

--- Casks/uzdoom.rb
+++ Casks/uzdoom.rb
@@ -1,6 +1,6 @@
 cask "uzdoom" do
   version "4.14.3"
-  sha256 "sha256:7fea2eb9de6a4694e56da9f7750f8f002d272891908b730020122b22beb59c0c"
+  sha256 "7fea2eb9de6a4694e56da9f7750f8f002d272891908b730020122b22beb59c0c"
 
   url "https://github.com/UZDoom/UZDoom/releases/download/#{version}/macOS-UZDoom-#{version}.zip"
   name "UZDoom"
