12102112813d8d341272bf25db7d68df55fd473d
Marcel <jsoservice@yahoo.com>
2025-12-02 00:18:33 -0500

    Add files via upload

---
+++ Casks/infekt.rb
@@ -0,0 +1,11 @@
+cask "infekt" do
+  version "2.0.0-alpha1"
+  sha256 "06cb4cdaacd5e0784087be3dd6def5552282fdb36d5928639addb535b568c9d0"
+
+  url "https://github.com/syndicodefront/infekt/releases/download/v2.0.0-alpha1/iNFekt-v#{version}-macos-aarch64.dmg"
+  name "Infekt"
+  desc "The ultimate best NFO viewer, ever"
+  homepage "https://github.com/UZDoom/UZDoom"
+
+  app "Infekt.app"
+end
