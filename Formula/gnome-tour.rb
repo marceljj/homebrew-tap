2380211dbcf8e5cb700adfc14a5880c266e58ea8
Marcel <jsoservice@yahoo.com>
2025-12-01 22:45:23 -0500

    Update gnome-tour.rb

--- Formula/gnome-tour.rb
+++ Formula/gnome-tour.rb
@@ -6,10 +6,10 @@ class GnomeTour < Formula
   license "GPL-3.0"
   version "49.0"
 
-  depends_on "cmake" => :build
   depends_on "desktop-file-utils"
   depends_on "glib"
   depends_on "gtk4"
+  depends_on "libadwaita"
   depends_on "meson"
   depends_on "rust"
 
