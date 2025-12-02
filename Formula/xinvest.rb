a8e32b4e6015343d1583ecc22247cbe196b0fca7
Marcel <jsoservice@yahoo.com>
2025-12-02 00:02:24 -0500

    Update xinvest.rb to use dynamic URL for version

--- Formula/xinvest.rb
+++ Formula/xinvest.rb
@@ -1,8 +1,8 @@
 class Xinvest < Formula
+  version "2.5.1"
   desc "A program which provides personal finance tracking and performance capabilities"
   homepage "https://x11cp.org/apps/xinvest"
-  url "https://marceljj.org/pkg/bin/xinvest-2.5.1.tar.gz"
-  version "2.5.1"
+  url "https://marceljj.org/pkg/bin/xinvest-#{version}.tar.gz"
   sha256 "2013d46d05739f48a0798a24b0e038e68940829deebdb720f26af7993d9381ef"
 
   depends_on arch: :arm64
