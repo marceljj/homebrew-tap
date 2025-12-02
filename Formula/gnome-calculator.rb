e874ed895d4334ce2feb87b64957c83b20e88f50
Marcel <jsoservice@yahoo.com>
2025-12-01 23:51:08 -0500

    Remove version specification from gnome-calculator.rb
    Removed the version specification for gnome-calculator.

--- Formula/gnome-calculator.rb
+++ Formula/gnome-calculator.rb
@@ -4,7 +4,6 @@ class GnomeCalculator < Formula
   url "https://gitlab.gnome.org/GNOME/gnome-calculator/-/archive/#{version}/gnome-calculator-#{version}.tar.gz"
   sha256 "ddb5410091c841d4e93c61570c4e5a68e5c7d0f2ae19ea81be8fd8d29255b9ca"
   license "GPL-3.0"
-  version "49.2"
 
   depends_on "blueprint-compiler"
   depends_on "glib"
