264bc0e5cae3a92a79de5af48cd4a0b60ff8356e
Marcel <jsoservice@yahoo.com>
2025-12-02 00:52:07 -0500

    Remove license from infekt.rb cask
    Removed license information from the infekt cask.

--- Casks/infekt.rb
+++ Casks/infekt.rb
@@ -1,7 +1,6 @@
 cask "infekt" do
   version "2.0.0-alpha1"
   sha256 "06cb4cdaacd5e0784087be3dd6def5552282fdb36d5928639addb535b568c9d0"
-  license "GPL-2.0"
 
   url "https://github.com/syndicodefront/infekt/releases/download/v#{version}/iNFekt-v#{version}-macos-aarch64.dmg"
   name "Infekt"
