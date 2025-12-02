fe87423e5cd1bcac824903ccecd28d41d1ce523d
Marcel <jsoservice@yahoo.com>
2025-12-02 00:01:50 -0500

    Update URL to use version variable

--- Formula/motti.rb
+++ Formula/motti.rb
@@ -1,9 +1,9 @@
 class Motti < Formula
+  version "3.1.1"
   desc "Conquer enemies' capitals by occupying and encircling their territories. The game map is an array of square cells."
   homepage "https://www.gnu.org/software/motti"
-  url "https://ftp.gnu.org/gnu/motti/motti-3.1.1.tar.gz"
+  url "https://ftp.gnu.org/gnu/motti/motti-#{version}.tar.gz"
   license "GPL-3.0"
-  version "3.1.1"
   sha256 "632d9a84d3b2e3eee4c9d9199d66d42576516ea6c291058c01830ae2fb7de429"
 
   depends_on "make"
