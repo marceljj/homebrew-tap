386c1c6a41391b90d42f589e9fd3282d927925b7
Marcel <jsoservice@yahoo.com>
2025-12-03 16:33:28 -0500

    Update infekt.rb

--- Casks/infekt.rb
+++ Casks/infekt.rb
@@ -1,7 +1,7 @@
 cask "infekt" do
   arch arm: "aarch64", intel: "intel"
   version "2.0.0-alpha1"
-  sha256 arm: "06cb4cdaacd5e0784087be3dd6def5552282fdb36d5928639addb535b568c9d0"
+  sha256 arm: "06cb4cdaacd5e0784087be3dd6def5552282fdb36d5928639addb535b568c9d0",
          intel: "c0867a52374843cbb34a67fd632881ad60b758262d66be5abb0e86b046c90fee"
   url "https://github.com/syndicodefront/infekt/releases/download/v#{version}/iNFekt-v#{version}-macos-#{arch}.dmg"
   name "Infekt"
