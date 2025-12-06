7dbaa33ad6af7d5d31498592c1c1b9a0af4c4bdd
Marcel <jsoservice@yahoo.com>
2025-12-05 19:43:48 -0500

    Update galculator.rb

--- Formula/galculator.rb
+++ Formula/galculator.rb
@@ -5,6 +5,7 @@ class Galculator < Formula
   url "https://github.com/galculator/galculator.git", branch: "master"
 
   depends_on "gtk+3"
+  depends_on "intltool"
   depends_on "libtool"
   depends_on "make"
 
