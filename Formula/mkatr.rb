1ab46a7e9911960c8ea098b0c673f68bfad3e3f8
Marcel <jsoservice@yahoo.com>
2025-12-11 00:41:43 -0500

    Add files via upload

---
+++ Formula/mkatr.rb
@@ -0,0 +1,20 @@
+class Mkatr < Formula
+  version "git-2024.9"
+  desc "ATR disk image creation tool"
+  homepage "https://github.com/dmsc/mkatr"
+  url "https://github.com/dmsc/mkatr.git", branch: "master"
+  license "GPL-2.0"
+
+  def install
+    system "make", "PREFIX=#{prefix}"
+    system "make", "install", "PREFIX=#{prefix}"
+    bin.install "mkatr"
+    bin.install "lsatr"
+    (share/"doc").install "LICENSE"
+    (share/"doc").install "README.md"
+  end
+
+  test do
+    system "false"
+  end
+end
