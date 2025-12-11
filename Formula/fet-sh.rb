7a519bf74184127bbd9aeaa6ddd89a388074104e
Marcel <jsoservice@yahoo.com>
2025-12-10 19:10:09 -0500

    Update fet-sh.rb

--- Formula/fet-sh.rb
+++ Formula/fet-sh.rb
@@ -1,6 +1,6 @@
 class FetSh < Formula
   version "1.0"
-  desc "A fetch written in posix shell without any external commands"
+  desc "A fetch written in POSIX shell without any external commands"
   homepage "https://github.com/eepykate/fet.sh"
   url "https://github.com/eepykate/fet.sh.git", branch: "master"
   license "ISC"
