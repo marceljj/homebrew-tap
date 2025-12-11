9e00261fbec8d5df269115cc2d7d338f0f5ca144
Marcel <jsoservice@yahoo.com>
2025-12-11 00:28:17 -0500

    Add files via upload

---
+++ Formula/emu2.rb
@@ -0,0 +1,16 @@
+class Emu2 < Formula
+  version "git-2025.11"
+  desc "Simple x86 and DOS emulator for the UNIX terminal"
+  homepage "https://github.com/dmsc/emu2"
+  url "https://github.com/dmsc/emu2.git", branch: "master"
+  license "GPL-2.0"
+
+  def install
+    system "make", "PREFIX=#{prefix}"
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    system "false"
+  end
+end
