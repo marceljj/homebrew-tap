b541214944f9e33ee1dd193d21ed099b7d665301
Marcel <jsoservice@yahoo.com>
2025-12-12 02:30:06 -0500

    Add files via upload

---
+++ Formula/stiv.rb
@@ -0,0 +1,17 @@
+class Stiv < Formula
+  version "git-2023.01"
+  desc "Suckless terminal image viewer"
+  homepage "https://github.com/radare/tiv"
+  url "https://github.com/dmsc/radare/tiv.git", branch: "master"
+  license "BSD-3-Clause"
+
+  def install
+    system "make", "PREFIX=#{prefix}"
+    bin.install "stiv"
+    bin.install "stiv-jpeg"
+  end
+
+  test do
+    system "false"
+  end
+end
