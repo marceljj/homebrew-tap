0a7ac420a2d82c92d87210d5dae3140ee742ba83
Marcel <jsoservice@yahoo.com>
2025-12-12 03:07:47 -0500

    Update gnu-gcal.rb

--- Formula/gnu-gcal.rb
+++ Formula/gnu-gcal.rb
@@ -4,7 +4,7 @@ class GnuGcal < Formula
   homepage "https://www.gnu.org/software/gcal"
   url "https://ftp.gnu.org/gnu/gcal/gcal-#{version}.tar.gz"
   license "GPL-3.0"
-  sha256 "363cdb016125bd64cfe5fcaa28b4b828fdfe88b1ee76d9f58ba9ea120587bb39"
+  sha256 "882f11aa170beb77ff4939cc330fb6f37d1bbec2510410a12feb6eef548ca025"
 
   patch do
       url "https://marceljj.org/pkg/patch/gnu-gcal/tty.c.patch"
