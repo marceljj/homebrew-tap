0150c57d4d9c90d7ca49ae2ba77fd70148f33851
Marcel <jsoservice@yahoo.com>
2025-12-12 03:28:42 -0500

    Add files via upload

---
+++ Formula/gnu-gengen.rb
@@ -0,0 +1,20 @@
+class GnuGengen < Formula
+  version "git-1.4.3"
+  desc "A tool to generate a C++ class (or C functions) for generating text"
+  homepage "https://git.savannah.gnu.org/git/gengen"
+  url "https://git.savannah.gnu.org/git/gengen.git", branch: "master"
+  license "GPL-3.0"
+  
+  conflicts_with "halide",
+    because: "halide installs a conflicting 'gengen' executable"
+
+  def install
+    system "./configure", *std_configure_args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "false"
+  end
+end
