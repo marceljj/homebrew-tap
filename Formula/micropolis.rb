ce24417149e4690182d1fe2d3e9bf6b54de9d0a4
Marcel <jsoservice@yahoo.com>
2025-12-13 04:24:47 -0500

    Add files via upload

---
+++ Formula/micropolis.rb
@@ -0,0 +1,18 @@
+class Micropolis < Formula
+  version "git-2025.07"
+  desc "Open-source version of SimCity"
+  homepage "https://git.zerfleddert.de/cgi-bin/gitweb.cgi/micropolis"
+  url "https://git.zerfleddert.de/git/micropolis.git", branch: "master"
+  license "GPL-3.0-or-later"
+
+  depends_on "tcl-tk"
+  
+  def install
+    system "make", "PREFIX=#{prefix}"
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    system "false"
+  end
+end
