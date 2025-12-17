8cb2ae0e8f75c1b459368d0c992ba258caa446bc
Marcel <jsoservice@yahoo.com>
2025-12-17 17:01:24 -0500

    Add files via upload

---
+++ Formula/audacious-full.rb
@@ -0,0 +1,88 @@
+class AudaciousFull < Formula
+  version "4.5.1"
+  desc "Lightweight and versatile audio player (full)"
+  homepage "https://audacious-media-player.org/"
+  license "BSD-2-Clause"
+  url "https://distfiles.audacious-media-player.org/audacious-#{version}.tar.bz2"
+  resource "plugins" do
+    url "https://distfiles.audacious-media-player.org/audacious-plugins-#{version}.tar.bz2"
+    end
+  end
+
+  depends_on "gettext" => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkgconf" => :build
+  depends_on "qttools" => :build
+  depends_on "faad2"
+  depends_on "ffmpeg"
+  depends_on "flac"
+  depends_on "fluid-synth"
+  depends_on "gdk-pixbuf"
+  depends_on "glib"
+  depends_on "gtk+-3.0"
+  depends_on "lame"
+  depends_on "libbs2b"
+  depends_on "libcue"
+  depends_on "libmodplug"
+  depends_on "libnotify"
+  depends_on "libogg"
+  depends_on "libopenmpt"
+  depends_on "libsamplerate"
+  depends_on "libsidplayfp"
+  depends_on "libsndfile"
+  depends_on "libsoxr"
+  depends_on "libvorbis"
+  depends_on "mpg123"
+  depends_on "neon"
+  depends_on "opusfile"
+  depends_on "qtbase"
+  depends_on "qtimageformats" => :no_linkage # for webp album covers
+  depends_on "qtmultimedia"
+  depends_on "qtsvg" => :no_linkage # for svg icons
+  depends_on "sdl2"
+  depends_on "wavpack"
+
+  uses_from_macos "curl"
+  uses_from_macos "zlib"
+
+  on_macos do
+    depends_on "gettext"
+  end
+
+  on_linux do
+    depends_on "alsa-lib"
+    depends_on "jack"
+    depends_on "libx11"
+    depends_on "libxml2"
+    depends_on "pulseaudio"
+  end
+
+  def install
+    odie "plugins resource needs to be updated" if build.stable? && version != resource("plugins").version
+
+    args = %w[
+      -Dlibarchive=true
+    ]
+
+    system "meson", "setup", "build", *args, *std_meson_args
+    system "meson", "compile", "-C", "build", "--verbose"
+    system "meson", "install", "-C", "build"
+
+    resource("plugins").stage do
+      args += %w[
+        -Dmpris2=false
+        -Dmac-media-keys=true
+      ]
+
+      ENV.prepend_path "PKG_CONFIG_PATH", lib/"pkgconfig"
+      system "meson", "setup", "build", *args, *std_meson_args
+      system "meson", "compile", "-C", "build", "--verbose"
+      system "meson", "install", "-C", "build"
+    end
+  end
+
+  test do
+    system bin/"audacious", "--help"
+  end
+end
