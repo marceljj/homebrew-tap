333dc608e8d4c8697d49f365248a278ffe38f381
Marcel <jsoservice@yahoo.com>
2025-12-18 17:57:29 -0500

    Update nip2.rb

--- Formula/nip2.rb
+++ Formula/nip2.rb
@@ -6,6 +6,8 @@ class Nip2 < Formula
   sha256 "b75e26eb3fb994faa988e8e07436cac2a4965c2cab08bef3968e817a866d76cd"
   license "GPL-2.0"
 
+  depends_on "automake"
+  
   def install
     system "./autogen.sh"
     system "./configure", *std_configure_args
