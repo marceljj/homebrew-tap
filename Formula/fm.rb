f6323273aa744519f1c1988f33ca2f7e49f099ca
Marcel <jsoservice@yahoo.com>
2025-12-18 18:32:28 -0500

    Update fm.rb

--- Formula/fm.rb
+++ Formula/fm.rb
@@ -5,13 +5,13 @@ class Fm < Formula
   url "https://github.com/euclio/fm.git", branch: "master"
   license "MIT"
 
+  depends_on "rust" => :build
   depends_on "glib"
   depends_on "gtk4"
   depends_on "gtksourceview5"
   depends_on "libadwaita"
   depends_on "libpanel"
   depends_on "poppler"
-  depends_on "rust"
 
   def install
     system "cargo", "install", *std_cargo_args
