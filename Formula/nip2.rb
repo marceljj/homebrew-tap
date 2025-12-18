8ba4921ce05f96de6340f7981507df44429f52f3
Marcel <jsoservice@yahoo.com>
2025-12-18 18:02:09 -0500

    Update nip2.rb

--- Formula/nip2.rb
+++ Formula/nip2.rb
@@ -5,8 +5,10 @@ class Nip2 < Formula
   url "https://github.com/libvips/nip2/releases/download/v#{version}/nip2-#{version}.tar.gz"
   sha256 "b75e26eb3fb994faa988e8e07436cac2a4965c2cab08bef3968e817a866d76cd"
   license "GPL-2.0"
-
+  
   depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "glib"
   
   def install
     system "./autogen.sh"
