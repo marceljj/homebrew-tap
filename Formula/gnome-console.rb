1b14f6684d4e5c610c4a7a47d07d322d0a713ae7
Marcel <jsoservice@yahoo.com>
2025-12-18 18:22:19 -0500

    Update gnome-console.rb

--- Formula/gnome-console.rb
+++ Formula/gnome-console.rb
@@ -7,6 +7,7 @@ class GnomeConsole < Formula
   license "GPL-3.0"
 
   depends_on "meson" => :build
+  depends_on "ninja" => :build
   depends_on "desktop-file-utils"
   depends_on "glib"
   depends_on "gtk4"
