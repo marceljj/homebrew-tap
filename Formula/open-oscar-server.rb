cd2d3c4d908a674b9cd86d2cdd782ad17e59b283
Marcel <jsoservice@yahoo.com>
2025-12-19 20:44:46 -0500

    Add files via upload

---
+++ Formula/open-oscar-server.rb
@@ -0,0 +1,18 @@
+class OpenOscarServer < Formula
+  version "0.21.0"
+  desc "Self-hostable instant messaging server compatible with classic AIM and ICQ clients"
+  homepage "https://github.com/mk6i/open-oscar-server"
+  url "https://github.com/mk6i/open-oscar-server/archive/refs/tags/v#{version}.tar.gz"
+  sha256 "e5f9fc668cf8662605c41f934593862047b4370c8f9e7c4a04f1764b18406c4b"
+  license "MIT"
+  
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args, "./cmd/server"
+  end
+
+  test do
+    system "false"
+  end
+end
