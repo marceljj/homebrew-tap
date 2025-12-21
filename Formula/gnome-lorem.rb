69cb765dd95101ab42aabbca71649ea608dc40ff
Marcel <jsoservice@yahoo.com>
2025-12-21 01:13:31 -0500

    Add files via upload

--- Formula/gnome-lorem.rb
+++ Formula/gnome-lorem.rb
@@ -2,8 +2,8 @@ class GnomeLorem < Formula
   version "1.6"
   desc "Simple app to generate the well-known Lorem Ipsum placeholder text"
   homepage "https://apps.gnome.org/Lorem"
-  url "https://marceljj.org/pkg/gnome-lorem-#{version}.tar.gz"
-  sha256 "eb08356872ecf8fe58b0637cc9098f4ab2060126ef2c2ba4dcf2ef3bef3d768f"
+  url "https://gitlab.gnome.org/World/design/lorem/-/archive/#{version}/lorem-#{version}.tar.gz"
+  sha256 "7da5ecc08cabf1bd2520bbe06561a886f1b01abe467a7c3e6bc15f79510998c1"
   license "GPL-3.0-or-later"
 
   depends_on "desktop-file-utils" => :build
@@ -17,6 +17,13 @@ class GnomeLorem < Formula
   def install
     ENV["DESTDIR"] = "/"
 
+    inreplace "Cargo.toml", "lorem", "gnome-lorem"
+    inreplace "meson.build" do |s|
+        s.gsub! "'lorem'", "'gnome-lorem'"
+        g.sub! "application_name = 'Lorem''", "application_name = 'GNOME-Lorem''"
+    end
+    inreplace "data/org.gnome.design.Lorem.desktop.in.in", "Exec=lorem", "Exec=gnome-lorem"
+    
     system "meson", "setup", "build", *std_meson_args
     system "meson", "compile", "-C", "build", "--verbose"
     system "meson", "install", "-C", "build"
