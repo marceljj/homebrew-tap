45a69295af5dc0eb057e3da6b81e66f5ab04c4ae
Marcel <jsoservice@yahoo.com>
2025-12-21 16:24:43 -0500

    Update xtt.rb

--- Formula/xtt.rb
+++ Formula/xtt.rb
@@ -3,11 +3,15 @@ class Xtt < Formula
   desc "A program which displays a timetable, one day at a time, between Monday and Friday."
   homepage "https://x11cp.org/apps/xtt"
   url "https://marceljj.org/pkg/xtt-#{version}.tar.gz"
-  sha256 "4df508ab5335bc47ba2a2637ce30588222ff320604b4c3fa650a54110d7a940b"
+  sha256 "2b28b66c351e96124f0b03e44fd1ae0e6dcc78b977d731d89adab821a8918de7"
 
   depends_on "imake" => :build
 
   def install
+    inreplace "xtt.c" do |s|
+      s.gsub! "<stdio.h>", "<stdio.h>\n#include <stdlib.h>"
+      s.gsub! "extern getenv()", "char *getenv(const char *name)"
+    
     system "xmkmf"
     system "make"
     bin.install "xtt"
