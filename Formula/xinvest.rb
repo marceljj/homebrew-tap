6afb8569e5e431e8be5ecbf23118faccb1b4d23c
Marcel <jsoservice@yahoo.com>
2025-12-21 12:46:43 -0500

    Add files via upload

--- Formula/xinvest.rb
+++ Formula/xinvest.rb
@@ -2,15 +2,18 @@ class Xinvest < Formula
   version "2.5.1"
   desc "A program which provides personal finance tracking and performance capabilities"
   homepage "https://x11cp.org/apps/xinvest"
-  url "https://marceljj.org/pkg/bin/xinvest-#{version}.tar.gz"
-  sha256 "2013d46d05739f48a0798a24b0e038e68940829deebdb720f26af7993d9381ef"
+  url "https://marceljj.org/pkg/xinvest-#{version}.tar.gz"
+  sha256 "5721b20acabb8da1a645ee8eb7f6336c5cdcfff493a4627e98789f8a73398a02"
 
-  depends_on arch: :arm64
-  depends_on :macos
-  
-  depends_on "openmotif"
+  depends_on "imake" => :build
+  depends_on "openmotif" => :build
 
   def install
+    inreplace "drawing.c", "static firsttime", "int static firsttime"
+    inreplace "portfolio.c", "<values.h>", "<float.h>"
+    inreplace "status.c", "static lineno", "int static lineno"
+    inreplace "xinvest.c", "<unistd.h>", "<unistd.h>\n#include <stdlib.h>"
+      
     bin.install "Xinvest"
     (lib/"X11/app-defaults").install "Xinvest.ad" => "Xinvest"
   end
