4f8f640e2f6ffcd9cddc64c40a5df92cc37df411
Marcel <jsoservice@yahoo.com>
2025-12-21 15:27:39 -0500

    Update xinvest.rb

--- Formula/xinvest.rb
+++ Formula/xinvest.rb
@@ -2,13 +2,13 @@ class Xinvest < Formula
   version "2.5.1"
   desc "A program which provides personal finance tracking and performance capabilities"
   homepage "https://x11cp.org/apps/xinvest"
-  url "https://marceljj.org/pkg/xinvest-#{version}.tar.gz"
-  sha256 "5721b20acabb8da1a645ee8eb7f6336c5cdcfff493a4627e98789f8a73398a02"
+  url "https://codeberg.org/x11cp/x11cp.git", branch "main"
 
   depends_on "imake" => :build
   depends_on "openmotif"
 
   def install
+    system "cd", "x11cp/src/xinvest"
     inreplace "drawing.c", "static firsttime", "int static firsttime"
     inreplace "Imakefile", "-fcommon", "-fcommon -w"
     inreplace "portfolio.c", "<values.h>", "<float.h>"
