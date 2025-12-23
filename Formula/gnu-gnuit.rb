526a6b71e32ef0b8d7994d038856ef65adde81eb
Marcel <jsoservice@yahoo.com>
2025-12-23 00:29:17 -0500

    Update gnu-gnuit.rb

--- Formula/gnu-gnuit.rb
+++ Formula/gnu-gnuit.rb
@@ -5,6 +5,8 @@ class GnuGnuit < Formula
   url "https://ftp.gnu.org/gnu/gnuit/gnuit-#{version}.tar.gz"
   license "GPL-3.0"
   sha256 "6b6e96db13bafa5ad35c735b2277699d4244088c709a3e134fb1a3e8c8a8557c"
+
+  depends_on "texi2html" => :build
   
   def install   
     system "./configure", *std_configure_args
