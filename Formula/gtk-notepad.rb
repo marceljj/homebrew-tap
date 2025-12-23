8d8b94eb5704c77b7fe737959abad4e1456621ff
Marcel <jsoservice@yahoo.com>
2025-12-22 20:51:20 -0500

    Add files via upload

---
+++ Formula/gtk-notepad.rb
@@ -0,0 +1,21 @@
+class GtkNotepad < Formula
+  version "git-2016.08"
+  desc "Basic text editor written in C/GTK"
+  homepage "https://github.com/JadedTuna/gtk-notepad"
+  url "https://github.com/JadedTuna/gtk-notepad.git", branch: "master"
+  license "GPL-2.0"
+
+  depends_on "glib"
+  depends_on "gtk+"
+  
+  def install
+    system "make", "all"
+    bin.install "gtk-notepad"
+    doc.install "LICENSE"
+    doc.install "README.md"
+  end
+
+  test do
+    system "false"
+  end
+end
