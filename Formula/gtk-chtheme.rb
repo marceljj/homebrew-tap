724979a9ce902d4190714ff056f520d483951ea4
Marcel <jsoservice@yahoo.com>
2025-12-22 21:34:59 -0500

    Add files via upload

---
+++ Formula/gtk-chtheme.rb
@@ -0,0 +1,22 @@
+class Gtk2fontsel < Formula
+  version "0.3.1"
+  desc "A font selection program for X11 using the GTK2 toolkit"
+  homepage "http://plasmasturm.org/code/gtk-chtheme"
+  url "http://plasmasturm.org/code/gtk-chtheme/gtk-chtheme-0.3.1.tar.bz2"
+  sha256 "26f4b6dd60c220d20d612ca840b6beb18b59d139078be72c7b1efefc447df844"
+  license "GPL-2.0"
+
+  depends_on "glib"
+  depends_on "gtk+"
+  
+  def install
+    system "make", "DESTDIR=#{prefix}"
+    system "make", "install"
+    doc.install "ChangeLog"
+    doc.install "COPYING"
+  end
+
+  test do
+    system "false"
+  end
+end
