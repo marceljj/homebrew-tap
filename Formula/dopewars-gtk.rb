f276ed2e37bfe7bc3a329d04eb8bcae8f4e7c69c
Marcel <jsoservice@yahoo.com>
2025-12-23 18:53:07 -0500

    Add files via upload

---
+++ Formula/dopewars-gtk.rb
@@ -0,0 +1,37 @@
+class DopewarsGtk < Formula
+  desc "Free rewrite of a game originally based on "Drug Wars" (with GTK3 support)"
+  version "1.6.2"
+  homepage "https://dopewars.sourceforge.io"
+  url "https://downloads.sourceforge.net/project/dopewars/dopewars/#{version}/dopewars-#{version}.tar.gz"
+  sha256 "623b9d1d4d576f8b1155150975308861c4ec23a78f9cc2b24913b022764eaae1"
+  license "GPL-2.0-or-later"
+
+  depends_on "pkgconf" => :build
+  
+  depends_on "glib"
+  depends_on "gtk+3"
+
+  uses_from_macos "curl"
+  uses_from_macos "ncurses"
+
+  on_macos do
+    depends_on "gettext"
+  end
+
+  def install
+    inreplace "src/Makefile.in", "$(dopewars_DEPENDENCIES)", ""
+    inreplace "src/Makefile.in", "chmod", "true"
+    inreplace "auxbuild/ltmain.sh", "need_relink=yes", "need_relink=no"
+    inreplace "src/plugins/Makefile.in", "LIBADD =", "LIBADD = -module -avoid-version"
+
+    system "./configure", "--enable-plugins",
+                          "--enable-networking",
+                          "--mandir=#{man}",
+                          *std_configure_args
+    system "make", "install", "chgrp=true"
+  end
+
+  test do
+    system bin/"dopewars", "-v"
+  end
+end
