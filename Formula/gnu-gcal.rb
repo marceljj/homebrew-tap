9999c65c821a44c34bc6757e10ca7df25407d7ce
Marcel <jsoservice@yahoo.com>
2025-12-22 23:06:05 -0500

    Update gnu-gcal.rb

--- Formula/gnu-gcal.rb
+++ Formula/gnu-gcal.rb
@@ -1,7 +1,7 @@
 class GnuGcal < Formula
   version "4.1"
-  desc "Conquer enemies' capitals by occupying and encircling their territories"
-  homepage "https://www.gnu.org/software/motti"
+  desc "A program for calculating and printing calendars"
+  homepage "https://www.gnu.org/software/gcal"
   url "https://ftp.gnu.org/gnu/gcal/gcal-#{version}.tar.gz"
   license "GPL-3.0"
   sha256 "363cdb016125bd64cfe5fcaa28b4b828fdfe88b1ee76d9f58ba9ea120587bb39"
