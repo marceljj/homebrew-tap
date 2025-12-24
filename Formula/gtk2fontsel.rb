6c0774518d636fc119e6287d78fbba11be625b98
Marcel <jsoservice@yahoo.com>
2025-12-23 21:03:46 -0500

    Update gtk2fontsel.rb

--- Formula/gtk2fontsel.rb
+++ Formula/gtk2fontsel.rb
@@ -2,10 +2,11 @@ class Gtk2fontsel < Formula
   version "0.1"
   desc "A font selection program for X11 using the GTK2 toolkit"
   homepage "https://gtk2fontsel.sourceforge.net"
-  url "https://downloads.sourceforge.net/project/gtk2fontsel/gtk2fontsel/0.1/gtk2fontsel-0.1.tar.gz"
+  url "https://downloads.sourceforge.net/project/gtk2fontsel/gtk2fontsel/#{version}/gtk2fontsel-#{version}.tar.gz"
   sha256 "5cd6396fc3c6e7f9bc358cc5ad30592ba446cdb0138a811216497b6453905a68"
   license "GPL-2.0"
 
+  depends_on "pkgconf" => :build
   depends_on "glib"
   depends_on "gtk+"
   
