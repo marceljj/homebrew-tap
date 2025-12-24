1466b46164c4dc68a8b92b579834caddc53578c6
Marcel <jsoservice@yahoo.com>
2025-12-23 20:59:59 -0500

    Update gnome-text-editor.rb

--- Formula/gnome-text-editor.rb
+++ Formula/gnome-text-editor.rb
@@ -11,6 +11,7 @@ class GnomeTextEditor < Formula
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "rust" => :build
+  depends_on "pkgconf" => :build
   depends_on "editorconfig"
   depends_on "glib"
   depends_on "gtk4"
