7913ff396a902d61fc14307e1c32d9cf863360c5
Marcel <jsoservice@yahoo.com>
2025-12-23 22:02:27 -0500

    Update neovim-gtk.rb

--- Formula/neovim-gtk.rb
+++ Formula/neovim-gtk.rb
@@ -6,7 +6,9 @@ class NeovimGtk < Formula
   sha256 "df4567bc687074e085b16199cd63edd0b19c12a3d66721f8c2c6021e709f3a21"
   license "GPL-3.0"
 
+  depends_on "pkgconf" => :build
   depends_on "rust" => :build
+  depends_on "glib"
 
   def install
     args = %w[
