586d93026093941e80306ea88b33b483b87b8c36
Marcel <jsoservice@yahoo.com>
2025-12-23 20:58:47 -0500

    Update gnome-font-viewer.rb

--- Formula/gnome-font-viewer.rb
+++ Formula/gnome-font-viewer.rb
@@ -8,6 +8,7 @@ class GnomeFontViewer < Formula
 
   depends_on "meson" => :build
   depends_on "ninja" => :build
+  depends_on "pkgconf" => :build
   depends_on "glib"
   depends_on "gtk4"
   depends_on "libadwaita"
