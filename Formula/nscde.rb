b9c0ce39c2495f22dedd709a0f8134865613d8f3
Marcel <jsoservice@yahoo.com>
2025-12-25 23:26:05 -0500

    Add files via upload

---
+++ Formula/nscde.rb
@@ -0,0 +1,19 @@
+class Nscde < Formula
+  version "2.3"
+  desc "Modern and functional CDE desktop based on FVWM"
+  homepage "https://github.com/NsCDE/NsCDE"
+  url "https://github.com/NsCDE/NsCDE/archive/refs/tags/#{version}.tar.gz"
+  sha256 "fc4fd5f16b901b865f44b7483fa01a28189b5d5b95766375e026cc317456b297"
+
+  depends_on "libx11"
+
+  def install
+    system "./configure", *std_configure_args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "false"
+  end
+end
