19ab8b08d989a1ae1d10478ee9eb9657020f4596
Marcel <jsoservice@yahoo.com>
2025-12-26 01:01:03 -0500

    Update fvwm3.rb

--- Formula/fvwm3.rb
+++ Formula/fvwm3.rb
@@ -1,7 +1,7 @@
 class Fvwm3 < Formula
   version "1.1.4"
   desc "A virtual window manager for the X windows system"
-  homepage "https://www.fvwm.org"
+  homepage "https://fvwm.org"
   url "https://github.com/fvwmorg/fvwm3/releases/download/#{version}/fvwm3-#{version}.tar.gz"
   sha256 "6df420ab492dcecd9d302880a9e97f689e9181db8850f5f7945e652bb8dd62a0"
   license all_of: ["GPL-2.0", "fvwm"]
