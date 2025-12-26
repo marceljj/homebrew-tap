c4432c6a6ae14891b8597e9623454c73e631ad32
Marcel <jsoservice@yahoo.com>
2025-12-26 04:42:11 -0500

    Update ratpoison.rb

--- Formula/ratpoison.rb
+++ Formula/ratpoison.rb
@@ -6,8 +6,6 @@ class Ratpoison < Formula
   sha256 "d98fa4be025ecca453c407ff311ab3949f29f20d6d8abedf8f0716b85fc8d1f1"
   license "GPL-2.0"
 
-  depends_on "libx11"
-
   def install
     system "./configure", *std_configure_args
     system "make"
