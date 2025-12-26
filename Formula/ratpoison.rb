706bbbc766a0605a5246b128a8478606faf34c57
Marcel <jsoservice@yahoo.com>
2025-12-26 04:35:21 -0500

    Add files via upload

---
+++ Formula/ratpoison.rb
@@ -0,0 +1,18 @@
+class Ratpoison < Formula
+  version "1.4.9"
+  desc "A simple Window Manager with no fat library dependencies, no fancy graphics, no window decorations, and no rodent dependence"
+  homepage "https://github.com/leahneukirchen/cwm"
+  url "http://download.savannah.nongnu.org/releases/ratpoison/ratpoison-#{version}.tar.xz"
+  sha256 "d98fa4be025ecca453c407ff311ab3949f29f20d6d8abedf8f0716b85fc8d1f1"
+  license "GPL-2.0"
+
+  def install
+    system "./configure" *std_configure_args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "false"
+  end
+end
