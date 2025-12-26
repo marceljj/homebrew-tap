28881c82f096a2175bdac03e5cc3e79b0e92132d
Marcel <jsoservice@yahoo.com>
2025-12-26 04:39:15 -0500

    Update ratpoison.rb

--- Formula/ratpoison.rb
+++ Formula/ratpoison.rb
@@ -1,13 +1,15 @@
 class Ratpoison < Formula
   version "1.4.9"
   desc "A simple Window Manager with no fat library dependencies, no fancy graphics, no window decorations, and no rodent dependence"
-  homepage "https://github.com/leahneukirchen/cwm"
+  homepage "https://www.nongnu.org/ratpoison"
   url "http://download.savannah.nongnu.org/releases/ratpoison/ratpoison-#{version}.tar.xz"
   sha256 "d98fa4be025ecca453c407ff311ab3949f29f20d6d8abedf8f0716b85fc8d1f1"
   license "GPL-2.0"
 
+  depends_on "libx11"
+
   def install
-    system "./configure" *std_configure_args
+    system "./configure", *std_configure_args
     system "make"
     system "make", "install"
   end
