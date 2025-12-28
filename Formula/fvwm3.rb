2a24c0133953083121609a3cdfea865e9694c537
Marcel <jsoservice@yahoo.com>
2025-12-27 22:55:17 -0500

    Update fvwm3.rb

--- Formula/fvwm3.rb
+++ Formula/fvwm3.rb
@@ -6,10 +6,10 @@ class Fvwm3 < Formula
   sha256 "6df420ab492dcecd9d302880a9e97f689e9181db8850f5f7945e652bb8dd62a0"
   license all_of: ["GPL-2.0", "fvwm"]
 
+  depends_on "asciidoctor"
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkgconf" => :build
-  depends_on "asciidoctor"
   depends_on "fribidi"
   depends_on "fontconfig"
   depends_on "freetype"
