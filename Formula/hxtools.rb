c502d473c6ecfa69c5ac9c65d6f15a7204580c81
Marcel <jsoservice@yahoo.com>
2025-12-28 22:48:50 -0500

    Update hxtools.rb

--- Formula/hxtools.rb
+++ Formula/hxtools.rb
@@ -18,7 +18,9 @@ class Hxtools < Formula
         s.gsub! "smm_hcdplay_LDADD = ${libHX_LIBS}", ""
         s.gsub! "doc/hcdplay.1 doc/hxtools.7", "doc/hxtools.7"
       end
-      inreplace "doc/hxtools.7", ".PP\n\fBhcdplay\fP(1) \(en control autonomous CDDA playback using Linux ioctls\n.PP", ".PP"
+      inreplace "doc/hxtools.7", ".PP
+      \fBhcdplay\fP(1) \(en control autonomous CDDA playback using Linux ioctls
+      .PP", ".PP"
     end
     
     system "./configure", *std_configure_args
