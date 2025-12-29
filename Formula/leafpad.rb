7b5f9321dac6ead63c688b826d0f236e9cfa31d4
Marcel <jsoservice@yahoo.com>
2025-12-29 02:21:32 -0500

    Add files via upload

---
+++ Formula/leafpad.rb
@@ -0,0 +1,21 @@
+class Leafpad < Formula
+  version "git-2025.03"
+  desc "GTK+ based simple text editor"
+  homepage "https://github.com/tarot231/leafpad"
+  url "https://github.com/tarot231/leafpad.git", branch: "main"
+  license "GPL-2.0"
+  
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+
+  def install
+    system "autoreconf", "-fi",
+    system "./configure", *std_configure_args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "false"
+  end
+end
