326799ec12a183745c0f2b19a5439a2b4b098381
Marcel <jsoservice@yahoo.com>
2025-12-29 02:47:10 -0500

    Update leafpad.rb

--- Formula/leafpad.rb
+++ Formula/leafpad.rb
@@ -9,7 +9,7 @@ class Leafpad < Formula
   depends_on "automake" => :build
   depends_on "gettext" => :build
   depends_on "intltool" => :build
-  depends_on "pkgconfig" => :build
+  depends_on "pkgconf" => :build
   depends_on "glib"
   depends_on "gtk+"
 
