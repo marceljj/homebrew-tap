d9846b03096311fb7726ac32422a197bd4b72a3e
Marcel <jsoservice@yahoo.com>
2025-12-29 17:52:38 -0500

    Update xcowsay.rb

--- Formula/xcowsay.rb
+++ Formula/xcowsay.rb
@@ -18,7 +18,7 @@ class Xcowsay < Formula
   depends_on "gtk+3"
 
   def install
-    inreplace "src/xcowfortune", "exec env PATH=$PATH fortune", "fortune"
+    inreplace "src/xcowfortune", "env PATH=$PATH fortune | env PATH=$PATH DISPLAY=$DISPLAY xcowsay $@", "fortune | xcowsay $@"
     on_macos do
       inreplace "src/display_cow.c", "setenv(\"GDK_BACKEND\", \"x11\", 1);", ""
     end
