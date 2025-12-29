fbaea8624de1051e13a5fcc046b77d04f7e729cc
Marcel <jsoservice@yahoo.com>
2025-12-28 22:29:35 -0500

    Update hxtools.rb

--- Formula/hxtools.rb
+++ Formula/hxtools.rb
@@ -6,6 +6,7 @@ class Hxtools < Formula
   sha256 "8877d62ad9a85d5b7968fe862065f98da17209394a8df520ae583876d46f3d9b"
   license all_of: ["BSD-2-Clause-Patent", "GPL-2.0-or-later", "MIT"]
 
+  depends_on "automake" => :build
   depends_on "libhx"
   depends_on "libxcb"
 
