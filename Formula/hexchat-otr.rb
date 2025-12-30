16d02010e8105f9a3860e6894f8c7a20180469b2
Marcel <jsoservice@yahoo.com>
2025-12-29 23:22:34 -0500

    Update hexchat-otr.rb

--- Formula/hexchat-otr.rb
+++ Formula/hexchat-otr.rb
@@ -19,16 +19,16 @@ class HexchatOtr < Formula
 
     system "meson", "setup", "build", *std_meson_args
     system "meson", "compile", "-C", "build", "--verbose"
-    share.install "build/src/otr.dylib"
+    (lib/"otr").install "build/src/otr.dylib"
   end
 
   def caveats
     <<~EOS
       The HexChat OTR plugin has been installed to:
-        #{share}/otr.dylib
+        #{lib}/otr/otr.dylib
 
       To enable it in HexChat, symlink it to the addons directory:
-        ln -s #{share}/otr.dylib ~/.config/hexchat/addons/otr.dylib
+        ln -s #{lib}/otr/otr.dylib ~/.config/hexchat/addons/otr.dylib
     EOS
   end
 
