66d64c00930b6119420dfe81c86be2197bf52f8a
Marcel <jsoservice@yahoo.com>
2025-12-30 23:29:40 -0500

    Add files via upload

---
+++ Formula/opendoas.rb
@@ -0,0 +1,27 @@
+class Opendoas < Formula
+  version "6.8.2"
+  desc "A portable fork of the OpenBSD 'doas' command"
+  homepage "https://github.com/Duncaen/OpenDoas"
+  url "https://github.com/Duncaen/OpenDoas/releases/download/v#{version}/opendoas-#{version}.tar.gz"
+  sha256 "28dca29adec5f4336465812d9e2243f599e62a78903de71c24f0cd6fe667edac"
+  license "ISC"
+
+  def install
+    system "./configure", *std_configure_args
+    system "make"
+    system "make", "install"
+  end
+
+  def caveats
+    <<~EOS
+      The 'doas' command will not work without an appropriate 'doas' file in:
+        /etc/pam.d/
+        
+      (the 'sudo' file located in the same directory is a good starting point for configuration)
+    EOS
+  end
+
+  test do
+    system "false"
+  end
+end
