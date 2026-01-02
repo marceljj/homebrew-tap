76814ff7431320b81de6d550918f41acb34a9dac
Marcel <jsoservice@yahoo.com>
2026-01-02 05:06:49 -0500

    Update sowm.rb

--- Formula/sowm.rb
+++ Formula/sowm.rb
@@ -3,13 +3,16 @@ class Sowm < Formula
   desc "An itsy bitsy floating window manager (220~ sloc!)"
   homepage "https://github.com/dylanaraps/sowm"
   url "https://github.com/dylanaraps/sowm/archive/refs/tags/#{version}.tar.gz"
+  sha256 "7e1d1391f8cf8d1b04458d0b4bb3b61684b535dddcc1dd7d37266e81efd57e02"
   license "MIT"
 
   depends_on "libx11"
 
   def install
     system "make", "PREFIX=#{prefix}"
-    system "make", "install", "PREFIX=#{prefix}"
+    bin.install "sowm"
+    doc.install "LICENSE.md"
+    doc.install "README.md"
   end
   
   test do
