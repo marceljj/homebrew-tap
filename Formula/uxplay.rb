9de7323878a129bba6acb5ebbb8820e943a838cb
Marcel <jsoservice@yahoo.com>
2026-01-02 02:36:15 -0500

    Update license and test command in uxplay.rb

--- Formula/uxplay.rb
+++ Formula/uxplay.rb
@@ -4,7 +4,7 @@ class Uxplay < Formula
   homepage "https://github.com/FDH2/UxPlay"
   url "https://github.com/FDH2/UxPlay/archive/refs/tags/v#{version}.tar.gz"
   sha256 "a2f41c5481c2c3c8f125c38f8142a99d69b21d727be816616b66dd96af9a9c63"
-  license "GPL-3.0"
+  license "GPL-3.0-or-later"
 
   depends_on "cmake" => :build
   depends_on "pkgconf" => :build
@@ -29,6 +29,6 @@ class Uxplay < Formula
   end
 
   test do
-    system "false"
+    system "uxplay", "-h"
   end
 end
