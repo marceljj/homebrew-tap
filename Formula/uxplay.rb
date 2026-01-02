13f84e068394f359f38ecfa0f705bec66ef247f9
Marcel <jsoservice@yahoo.com>
2026-01-02 03:14:12 -0500

    Update uxplay.rb

--- Formula/uxplay.rb
+++ Formula/uxplay.rb
@@ -24,8 +24,9 @@ class Uxplay < Formula
         s.gsub! "(hardware) v4l2h264dec", "(hardware) nvdec"
         s.gsub! "nvdec, nvh264dec", "nvh264dec"
         s.gsub! "nvh264dec,v4l2h264dec", "nvh264dec"
-        s.gsub! "          another choice when using v4l2h264dec: v4l2convert", ""
-        s.gsub! "Use Video4Linux2 for GPU hardware h264 decoding", "Disabled on this system."
+        s.gsub! "printf(/"          another choice when using v4l2h264dec: v4l2convert\n/");", ""
+        s.gsub! "printf(/"-v4l2     Use Video4Linux2 for GPU hardware h264 decoding\n/");", ""
+      end
     end
     
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args
