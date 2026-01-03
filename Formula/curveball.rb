cea6e53bdb23ccd5e9d4f48699329952110274dd
Marcel <jsoservice@yahoo.com>
2026-01-03 00:39:41 -0500

    Add files via upload

---
+++ Formula/curveball.rb
@@ -0,0 +1,18 @@
+class Curveball < Formula
+  version "1.0.0"
+  desc "Curve generator for Neverball"
+  homepage "https://github.com/MightyBurger/curveball"
+  url "https://github.com/MightyBurger/curveball/archive/refs/tags/v#{version}.tar.gz"
+  sha256 "3ad3bb84043f360efe9ae4fa9e4d62f8fb257ae98fd239af1aac8c5433501980"
+  license all_of: ["Apache-2.0", "MIT"]
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    system "false"
+  end
+end
