1a2bb0332e2a870e06507599882c5f031bb523c5
Marcel <jsoservice@yahoo.com>
2026-01-03 23:48:02 -0500

    Add files via upload

---
+++ Formula/pcb-rnd.rb
@@ -0,0 +1,23 @@
+class PcbRnd < Formula
+  version "3.1.7b"
+  desc "A free/open source, flexible, modular Printed Circuit Board editor"
+  homepage "http://repo.hu/projects/pcb-rnd"
+  url "http://repo.hu/projects/pcb-rnd/releases/pcb-rnd-#{version}.tar.gz"
+  sha256 "54e74f71ae6c3f87a4b44feed6874d3996a2f6853423b139dcd6d05138505ab6"
+  license "GPL-2.0"
+
+  depends_on "pkgconf" => :build
+  depends_on "glib"
+  depends_on "gtk+"
+  depends_on "librnd"
+
+  def install
+    system "./configure", *std_configure_args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "false"
+  end
+end
