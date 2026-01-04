58b467df23993a71471230a492fe7aa99582ade7
Marcel <jsoservice@yahoo.com>
2026-01-04 18:59:54 -0500

    Update mgdiff.rb

--- Formula/mgdiff.rb
+++ Formula/mgdiff.rb
@@ -2,18 +2,20 @@ class Mgdiff < Formula
   version "1.0"
   desc "A graphical frontend to the diff command, written using Motif"
   homepage "https://x11cp.org/apps/mgdiff"
-  url "https://marceljj.org/pkg/mgdiff-#{version}.tar.gz"
-  sha256 "8b0789ec1715c98d7b2eaaa95666373aac2f6ca3ca498e1a9fe64e38098f32b8"
+  url "https://codeberg.org/x11cp/x11cp.git", branch: "main"
+  license :public_domain
 
   depends_on "imake" => :build
   depends_on "openmotif"
 
   def install
-    system "xmkmf"
-    system "make"
-    bin.install "mgdiff"
-    man1.install "mgdiff.man" => "mgdiff.1"
-    (etc/"X11/app-defaults").install "Mgdiff.ad" => "Mgdiff"
+    Dir.chdir("src/mgdiff") do
+      system "xmkmf"
+      system "make"
+      bin.install "mgdiff"
+      man1.install "mgdiff.man" => "mgdiff.1"
+      (etc/"X11/app-defaults").install "Mgdiff.ad" => "Mgdiff"
+    end
   end
 
   test do
