08f10f875a686df706dbd06c8369636b2f70eb73
Marcel <jsoservice@yahoo.com>
2026-01-04 00:35:17 -0500

    Add files via upload

---
+++ Formula/camv-rnd.rb
@@ -0,0 +1,25 @@
+class CamvRnd < Formula
+  version "1.1.6"
+  desc "A free/open source, small, flexible viewer for PCB-related CAM file formats"
+  homepage "http://www.repo.hu/projects/camv-rnd"
+  url "http://www.repo.hu/projects/camv-rnd/releases/camv-rnd-#{version}.tar.gz"
+  sha256 "da195499d456f8663c50d77cf3d7d0512476c48949a90b486f8df69b8bdce1b6"
+  license "GPL-2.0"
+
+  depends_on "pkgconf" => :build
+  depends_on "glib"
+  depends_on "gtk+"
+  depends_on "librnd"
+
+  def install
+    ENV.append "LIBRND_PREFIX", "#{Formula['librnd'].opt_prefix}"
+    
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "false"
+  end
+end
---
+++ Formula/sch-rnd.rb
@@ -0,0 +1,25 @@
+class SchRnd < Formula
+  version "1.0.10"
+  desc "A simple, modular, scriptable schematics editor"
+  homepage "http://www.repo.hu/projects/sch-rnd"
+  url "http://www.repo.hu/projects/sch-rnd/releases/sch-rnd-#{version}.tar.gz"
+  sha256 "40e8503c6f3598c51ebe83e6a9109a8e137d093ebfe144f1c590d1ffa01c44f1"
+  license "GPL-2.0"
+
+  depends_on "pkgconf" => :build
+  depends_on "glib"
+  depends_on "gtk+"
+  depends_on "librnd"
+
+  def install
+    ENV.append "LIBRND_PREFIX", "#{Formula['librnd'].opt_prefix}"
+    
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "false"
+  end
+end
