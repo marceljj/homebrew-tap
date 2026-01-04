747066dc521e87b5dc007da0453baa07c4f4b592
Marcel <jsoservice@yahoo.com>
2026-01-03 23:49:49 -0500

    Update librnd.rb

--- Formula/librnd.rb
+++ Formula/librnd.rb
@@ -11,6 +11,7 @@ class Librnd < Formula
   depends_on "gtk+"
   depends_on "libepoxy"
   depends_on "libgd"
+  depends_on "librnd"
 
   def install
     system "./configure", "--disable-hid_lesstif", "--prefix=#{prefix}"
