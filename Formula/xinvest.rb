c041612231a3080ef8d4513db556e5468e66f267
Marcel <jsoservice@yahoo.com>
2026-01-04 18:51:29 -0500

    Update xinvest.rb

--- Formula/xinvest.rb
+++ Formula/xinvest.rb
@@ -2,27 +2,28 @@ class Xinvest < Formula
   version "2.5.1"
   desc "A program which provides personal finance tracking and performance capabilities"
   homepage "https://x11cp.org/apps/xinvest"
-  url "https://marceljj.org/pkg/xinvest-#{version}.tar.gz"
-  sha256 "5721b20acabb8da1a645ee8eb7f6336c5cdcfff493a4627e98789f8a73398a02"
+  url "https://codeberg.org/x11cp/x11cp.git", branch: "main"
 
   depends_on "imake" => :build
   depends_on "openmotif"
 
   def install
-    inreplace "drawing.c", "static firsttime", "int static firsttime"
-    inreplace "Imakefile", "-fcommon", "-fcommon -w"
-    inreplace "portfolio.c", "<values.h>", "<float.h>"
-    inreplace "status.c", "static lineno", "int static lineno"
-    inreplace "xinvest.c", "<unistd.h>", "<unistd.h>\n#include <stdlib.h>"
+    Dir.chdir("src/xinvest") do
+      inreplace "drawing.c", "static firsttime", "int static firsttime"
+      inreplace "Imakefile", "-fcommon", "-fcommon -w"
+      inreplace "portfolio.c", "<values.h>", "<float.h>"
+      inreplace "status.c", "static lineno", "int static lineno"
+      inreplace "xinvest.c", "<unistd.h>", "<unistd.h>\n#include <stdlib.h>"
 
-    system "touch", "Xinvest.man"
-    system "xmkmf"
-    system "make"
-    bin.install "Xinvest"
-    (lib/"X11/app-defaults").install "Xinvest.ad" => "Xinvest"
-    doc.install "CHANGES"
-    doc.install "README"
-    doc.install "TODO"
+      system "touch", "Xinvest.man"
+      system "xmkmf"
+      system "make"
+      bin.install "Xinvest"
+      (lib/"X11/app-defaults").install "Xinvest.ad" => "Xinvest"
+      doc.install "CHANGES"
+      doc.install "README"
+      doc.install "TODO"
+    end
   end
 
   test do
