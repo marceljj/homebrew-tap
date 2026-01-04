6e0c761b39cb843ca3385becc63476e759f3d637
Marcel <jsoservice@yahoo.com>
2026-01-04 18:58:57 -0500

    Update xtt.rb

--- Formula/xtt.rb
+++ Formula/xtt.rb
@@ -2,15 +2,17 @@ class Xtt < Formula
   version "1.0"
   desc "A program which displays a timetable, one day at a time, between Monday and Friday."
   homepage "https://x11cp.org/apps/xtt"
-  url "https://marceljj.org/pkg/xtt-#{version}.tar.gz"
-  sha256 "2b28b66c351e96124f0b03e44fd1ae0e6dcc78b977d731d89adab821a8918de7"
+  url "https://codeberg.org/x11cp/x11cp.git", branch: "main"
+  license :public_domain
 
   depends_on "imake" => :build
 
   def install
-    inreplace "xtt.c" do |s|
-      s.gsub! "<stdio.h>", "<stdio.h>\n#include <stdlib.h>"
-      s.gsub! "extern getenv()", "char *getenv(const char *name)"
+    Dir.chdir("src/xtt")
+      inreplace "xtt.c" do |s|
+        s.gsub! "<stdio.h>", "<stdio.h>\n#include <stdlib.h>"
+        s.gsub! "extern getenv()", "char *getenv(const char *name)"
+      end
     end
     
     system "xmkmf"
