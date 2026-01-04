bfbfb06a570a630eea9761a5a54773431f45496e
Marcel <jsoservice@yahoo.com>
2026-01-04 18:57:05 -0500

    Update xmtoolbar.rb

--- Formula/xmtoolbar.rb
+++ Formula/xmtoolbar.rb
@@ -2,19 +2,22 @@ class Xmtoolbar < Formula
   version "1.2"
   desc "An ancient Motif-based toolbar for X11"
   homepage "https://x11cp.org/apps/xmtoolbar"
-  url "https://marceljj.org/pkg/xmtoolbar-#{version}.tar.gz"
-  sha256 "806af278d0d102f312c6caec49648b6dc4173e01093fb40cd4032f2abf280e5a"
+  url "https://codeberg.org/x11cp/x11cp.git", branch: "main"
+  license :public_domain
   
   depends_on "openmotif"
 
   def install
-    inreplace "toolbar.h", "DEFAULT_PIX     \"default.xpm\"", "DEFAULT_PIX     \"#{share}/xmtoolbar/default.xpm\""
+    Dir.chdir("src/xmtoolbar") do
+      inreplace "toolbar.h", "DEFAULT_PIX     \"default.xpm\"", "DEFAULT_PIX     \"#{share}/xmtoolbar/default.xpm\""
     
-    system "make", "-f", "makefile.simple"
-    bin.install "xmtoolbar"
-    doc.install "README"
-    (share/"xmtoolbar").install "default.xpm"
-    (etc/"X11/app-defaults").install "Xmtoolbar.ad" => "Xmtoolbar"
+      system "make", "-f", "makefile.simple"
+      bin.install "xmtoolbar"
+      doc.install "README"
+      (share/"xmtoolbar").install "default.xpm"
+      (share/"xmtoolbar").install "space.xpm"
+      (etc/"X11/app-defaults").install "Xmtoolbar.ad" => "Xmtoolbar"
+    end
   end
 
   test do
