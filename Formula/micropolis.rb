b6b2086178265f41629f418d897de9cbac45ddb6
Marcel <jsoservice@yahoo.com>
2026-01-04 23:34:20 -0500

    Add files via upload

---
+++ Formula/micropolis.rb
@@ -0,0 +1,18 @@
+class Micropolis < Formula
+  version "0.9.12"
+  desc "The city simulator"
+  homepage "https://git.zerfleddert.de/git/micropolis"
+  url "https://git.zerfleddert.de/git/micropolis.git", branch: "main"
+  license "GPL-3.0"
+
+  depends_on "libx11"
+
+  def install
+    system "make", "PREFIX=#{prefix}"
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+  
+  test do
+    system "false"
+  end
+end
