a5161f9fa2db1e729f8e546f589b98dc11bf9c84
Marcel <jsoservice@yahoo.com>
2026-01-06 01:37:00 -0500

    Update perfect-dark.rb

--- Formula/perfect-dark.rb
+++ Formula/perfect-dark.rb
@@ -12,7 +12,7 @@ class PerfectDark < Formula
   
   def install
     system "cmake", "-S", ".", "-G", "Unix Makefiles", "-DCMAKE_OSX_ARCHITECTURES=arm64", "-B", "build", *std_cmake_args
-    system "cmake", "--target", "pd", "--clean-first"
+    system "cmake --build build --target pd -j4 --clean-first"
     bin.install "build/pd.arm64" => :build
   end
 
