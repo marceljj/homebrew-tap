5d81f30f1fc9e3371828dadf22850224033078fb
Marcel <jsoservice@yahoo.com>
2026-01-06 02:29:44 -0500

    Update libdockapp.rb

--- Formula/libdockapp.rb
+++ Formula/libdockapp.rb
@@ -1,9 +1,9 @@
 class Libdockapp < Formula
   version "0.6.4"
-  desc "imple library that eases the creation of Window Maker dock apps"
+  desc "Simple library that eases the creation of Window Maker dock apps"
   homepage "https://www.dockapps.net/libdockapp"
   url "https://repo.or.cz/dockapps.git/snapshot/a4cd7361114ed8327982183409f6a12154684c85.tar.gz"
-  sha256 "d44f4ffccbdf0981bb2a754d2cf14e5515cf4a9893d0a4769ffc8499aedf2bd3"
+  sha256 "29143c5feed4e8e5bc6cfa0a08f478c2f9aeab5e3f026d7c4b63ae17c3face90"
   license "MIT"
 
   depends_on "autoconf" => :build
