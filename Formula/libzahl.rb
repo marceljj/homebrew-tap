c22f2948896b8a357c4e9f1332e309ce8b0560ea
Marcel <jsoservice@yahoo.com>
2026-01-06 02:08:10 -0500

    Update libzahl.rb

--- Formula/libzahl.rb
+++ Formula/libzahl.rb
@@ -3,14 +3,16 @@ class Libzahl < Formula
   desc "Big integer library"
   homepage "https://libs.suckless.org/libzahl"
   url "https://git.suckless.org/libzahl", branch: "master"
+  sha256 "b13042e0ba7944bb72a2aae9163279e48f7d3e01e4776bd48727cd12d0b0d462"
   license "ISC"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   
   def install
-    system "make", "PREFIX=#{prefix}"
-    system "make", "install", "PREFIX=#{prefix}"
+    inreplace "config.mk", "/usr/local", "#{prefix}"
+    system "make"
+    system "make", "install"
   end
 
   test do
