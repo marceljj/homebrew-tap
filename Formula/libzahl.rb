5266890ac7cf365b132c817d7a280a43e580683c
Marcel <jsoservice@yahoo.com>
2026-01-06 02:15:44 -0500

    Update libzahl.rb

--- Formula/libzahl.rb
+++ Formula/libzahl.rb
@@ -2,7 +2,7 @@ class Libzahl < Formula
   version "1.0"
   desc "Big integer library"
   homepage "https://libs.suckless.org/libzahl"
-  url "https://git.suckless.org/libzahl", branch: "master"
+  url "https://git.suckless.org/libzahl"
   sha256 "b13042e0ba7944bb72a2aae9163279e48f7d3e01e4776bd48727cd12d0b0d462"
   license "ISC"
 
