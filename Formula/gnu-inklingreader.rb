5c5750786e678898d4e21ebf0105ce7f6bb0b99c
Marcel <jsoservice@yahoo.com>
2026-01-05 22:30:46 -0500

    Update gnu-inklingreader.rb

--- Formula/gnu-inklingreader.rb
+++ Formula/gnu-inklingreader.rb
@@ -9,6 +9,7 @@ class GnuInklingreader < Formula
   depends_on "pkgconf" => :build
   depends_on "glib"
   depends_on "gtk+3"
+  depends_on "librsvg"
   
   def install
     system "./configure", *std_configure_args
