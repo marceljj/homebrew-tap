d0f2023cf998c7e6a55f7b2993d9532755bad8f8
Marcel <jsoservice@yahoo.com>
2026-01-07 23:53:16 -0500

    Update and rename xarclock.rb to bclock.rb

---
+++ Formula/bclock.rb
@@ -0,0 +1,23 @@
+class Bclock < Formula
+  version "1.0"
+  desc "A round, analog X11 clock"
+  homepage "https://x11cp.org/apps/bclock"
+  url "https://codeberg.org/x11cp/x11cp.git", branch: "main"
+  license :public_domain
+
+  depends_on "imake" => :build
+  depends_on "libx11"
+  
+  def install
+    Dir.chdir("src/bclock") do
+      system "xmkmf"
+      system "make"
+      system "make", "install"
+      doc.install "README"
+    end
+  end
+
+  test do
+    system "false"
+  end
+end
