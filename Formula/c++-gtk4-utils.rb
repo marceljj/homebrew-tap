8a103bf26fb312a6b421bbe51a8e92a49be383c4
Marcel <jsoservice@yahoo.com>
2026-01-10 00:08:08 -0500

    Add files via upload

---
+++ Formula/c++-gtk4-utils.rb
@@ -0,0 +1,22 @@
+class CxxGtk4Utils < Formula
+  version "2.2.20"
+  desc "lightweight library containing a number of classes and functions for programming GTK4 programs using C++ in POSIX environments"
+  homepage "https://cxx-gtk-utils.sourceforge.net"
+  url "https://downloads.sourceforge.net/project/cxx-gtk-utils/cxx-gtk-utils/#{version}/c%2B%2B-gtk-utils-#{version}.tar.gz"
+  sha256 "a64e03e5948d074f8309b036ca0acaf9ae4025c19cef6174f2319c504fb32d8c"
+  license "GPL-2.0-or-later"
+
+  depends_on "pkgconf" => :build
+  depends_on "glib"
+  depends_on "gtk+4"
+
+  def install
+    system "./configure-gtk4", *std_configure_args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "false"
+  end
+end
