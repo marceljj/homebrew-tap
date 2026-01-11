0a9c18f679f6c4075b4c9722d123be8448f906e1
Marcel <jsoservice@yahoo.com>
2026-01-11 15:26:08 -0500

    Add files via upload

---
+++ Formula/udu.rb
@@ -0,0 +1,26 @@
+class Udu < Formula
+  version "0.6.91"
+  desc "Extremely fast command-line disk usage analyzer"
+  homepage "https://github.com/gnualmalki/udu"
+  url "https://github.com/gnualmalki/udu/archive/refs/tags/udu@#{version}.tar.gz"
+  sha256 "7035f9fb518b2cb2ab4d9b5ba3e56c1a75cda1b98e07a2fbb66c9935d61d57e6"
+  license "GPL-3.0-or-later"
+
+  depends_on "libomp"
+
+  conflicts_with "uutils-coreutils", because: "uutils-coreutils installs a conflicting 'udu' binary"
+  
+  def install
+    inreplace "Makefile", "-include omp.mk", ""
+    system "make", "CFLAGS=-fopenmp", "LDFLAGS=-fopenmp"
+    bin.install "udu"
+    man1.install "udu.1"
+    doc.install "BENCHMARKS.md"
+    doc.install "COPYING"
+    doc.install "README.md"
+  end
+
+  test do
+    system "false"
+  end
+end
