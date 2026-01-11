f68a14a426c5ba3a803215dad35b7090448f9fe0
Marcel <jsoservice@yahoo.com>
2026-01-11 15:07:58 -0500

    Update udu.rb

--- Formula/udu.rb
+++ Formula/udu.rb
@@ -3,15 +3,14 @@ class Udu < Formula
   desc "Extremely fast command-line disk usage analyzer"
   homepage "https://github.com/gnualmalki/udu"
   url "https://github.com/gnualmalki/udu/archive/refs/tags/udu@#{version}.tar.gz"
+  sha256 "7035f9fb518b2cb2ab4d9b5ba3e56c1a75cda1b98e07a2fbb66c9935d61d57e6"
   license "GPL-3.0-or-later"
 
   depends_on "libomp"
   
   def install
-    system "make", "PREFIX=#{prefix}"
+    system "make", "CFLAGS=-fopenmp", "LDFLAGS=-fopenmp", "PREFIX=#{prefix}"
     system "make", "install", "PREFIX=#{prefix}"
-    doc.install "LICENSE"
-    doc.install "README.md"
   end
 
   test do
