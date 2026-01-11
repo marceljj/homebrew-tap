248cf1b0a680472f61300be3816bfd5961ffeefb
Marcel <jsoservice@yahoo.com>
2026-01-10 22:57:30 -0500

    Update links-x11.rb

--- Formula/links-x11.rb
+++ Formula/links-x11.rb
@@ -1,7 +1,7 @@
 class LinksX11 < Formula
   version "2.30"
   desc "Lynx-like WWW browser that supports tables, menus, etc. (with X11 support)"
-  homepage "https://links.twibright.com/"
+  homepage "https://links.twibright.com"
   url "https://links.twibright.com/download/links-#{version}.tar.bz2"
   sha256 "c4631c6b5a11527cdc3cb7872fc23b7f2b25c2b021d596be410dadb40315f166"
   license "GPL-2.0-or-later" => { with: "openvpn-openssl-exception" }
