e2acdf11cf017ec245642856a8031fc202ff6501
Marcel <jsoservice@yahoo.com>
2026-01-11 21:20:12 -0500

    Update td-lib.rb

--- Formula/td-lib.rb
+++ Formula/td-lib.rb
@@ -3,6 +3,7 @@ class TdLib < Formula
   desc "Tom Dickey's UNIX library"
   homepage "https://invisible-island.net"
   url "https://invisible-island.net/datafiles/release/td_lib.tar.gz"
+  sha256 "8854186a30eb4fd842397f54afe9309bfa53607bf60a9cf8e0b2a6b7231a35a3"
   license "MIT"
 
   def install
