fe925b6975fb561967edef9902f8ae4240467c23
Marcel <jsoservice@yahoo.com>
2026-01-11 22:05:22 -0500

    Update ded.rb

--- Formula/ded.rb
+++ Formula/ded.rb
@@ -6,6 +6,7 @@ class Ded < Formula
   sha256 "b5bfb0850a70f3b1e64c9736e73d00788442bfc4c273b5b3d13f68f454a8c530"
   license "MIT"
 
+  depends_on "pkgconf" => :build
   depends_on "td-lib"
   
   def install
