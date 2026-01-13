46b31d2a9f6f6cbe8a2211e0048d87be9792ce34
Marcel <jsoservice@yahoo.com>
2026-01-12 23:10:59 -0500

    Update dicom-rs.rb

--- Formula/dicom-rs.rb
+++ Formula/dicom-rs.rb
@@ -4,12 +4,18 @@ class DicomRs < Formula
   homepage "https://dicom-rs.github.io"
   url "https://github.com/Enet4/dicom-rs/archive/refs/tags/v#{version}.tar.gz"
   sha256 "e1ab480fb2c608b6a15a45afc0b9acf96435a786e254d390dc21f359ce15d711"
-  license all_of: ["Apache-2.0", "MIT"]
+  license any_of: ["Apache-2.0", "MIT"]
 
   depends_on "rust" => :build
 
   def install
-    system "cargo", "install", *std_cargo_args
+    system "cargo", "build", "--locked", "--release"
+    bin.install "target/release/dicom-*"
+    doc.install "CODE_OF_CONDUCT.md"
+    doc.install "CONTRIBUTING.md"
+    doc.install "LICENSE-APACHE"
+    doc.install "LICENSE-MIT"
+    doc.install "README.md"
   end
 
   test do
