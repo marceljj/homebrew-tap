b5644be521f246381ad3511f862c32ff921cd755
Marcel <jsoservice@yahoo.com>
2026-01-12 22:19:59 -0500

    Update gofish.rb

--- Formula/gofish.rb
+++ Formula/gofish.rb
@@ -3,6 +3,7 @@ class Gofish < Formula
   desc "A lightweight gopher server"
   homepage "https://gofish.sourceforge.net"
   url "https://downloads.sourceforge.net/project/gofish/gofish/#{version}/gofish-#{version}.tar.gz"
+  sha256 "1ab8390ec9b3373a298eee564c8b10b3527b22f86f044c9427d32065e9b6252f"
   license "GPL-2.0"
 
   def install
