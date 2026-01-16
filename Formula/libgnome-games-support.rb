0f2bf357395358b596c7218ba72263f54e61e2aa
Marcel <jsoservice@yahoo.com>
2026-01-16 02:41:18 -0500

    Add files via upload

---
+++ Formula/libgnome-games-support.rb
@@ -0,0 +1,27 @@
+class LibgnomeGamesSupport < Formula
+  version "2.0.2"
+  desc "A small library for playing system sounds"
+  homepage "https://gitlab.gnome.org/GNOME/libgnome-games-support"
+  url "https://gitlab.gnome.org/GNOME/libgnome-games-support/-/archive/#{version}/libgnome-games-support-#{version}.tar.gz"
+  sha256 "bf36baf14e6aa0388e4c4addf8c35a154f1c2252986dbae06e5c6f6187b531c4"
+  license any_of: ["GPL-3.0", "LGPL-3.0"]
+
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkgconf" => :build
+  depends_on "glib"
+  depends_on "gtk4"
+  depends_on "libadwaita"
+
+  def install
+    ENV["DESTDIR"] = "/"
+
+    system "meson", "setup", "build", *std_meson_args
+    system "meson", "compile", "-C", "build", "--verbose"
+    system "meson", "install", "-C", "build"
+  end
+
+  test do
+    system "false"
+  end
+end
