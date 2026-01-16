19cc60649d774989e244b866f8c84e791af11ea1
Marcel <jsoservice@yahoo.com>
2026-01-16 02:52:52 -0500

    Update gnome-nibbles.rb

--- Formula/gnome-nibbles.rb
+++ Formula/gnome-nibbles.rb
@@ -9,7 +9,7 @@ class GnomeNibbles < Formula
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkgconf" => :build
-  depends_on "valac" => :build
+  depends_on "vala" => :build
   depends_on "glib"
   depends_on "gsound"
   depends_on "gtk4"
