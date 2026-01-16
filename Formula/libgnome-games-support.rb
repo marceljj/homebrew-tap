cc94c3c7fd2fff2d2a22f8e4cb0b714929bf4298
Marcel <jsoservice@yahoo.com>
2026-01-16 02:43:09 -0500

    Update libgnome-games-support.rb

--- Formula/libgnome-games-support.rb
+++ Formula/libgnome-games-support.rb
@@ -9,6 +9,7 @@ class LibgnomeGamesSupport < Formula
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkgconf" => :build
+  depends_on "vala" => :build
   depends_on "glib"
   depends_on "gtk4"
   depends_on "libadwaita"
