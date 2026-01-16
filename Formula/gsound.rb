974f34c83661eb04a212e1d5f441ba9f222a376a
Marcel <jsoservice@yahoo.com>
2026-01-16 02:23:19 -0500

    Update gsound.rb

--- Formula/gsound.rb
+++ Formula/gsound.rb
@@ -9,6 +9,7 @@ class Gsound < Formula
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkgconf" => :build
+  depends_on "vala" => :build
   depends_on "libcanberra"
 
   def install
