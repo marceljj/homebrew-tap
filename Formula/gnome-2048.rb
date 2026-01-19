a3ca0b572b47114a97bbc2e120e4589c0a2d29d1
Marcel <jsoservice@yahoo.com>
2026-01-19 06:47:43 -0500

    Update gnome-2048.rb

--- Formula/gnome-2048.rb
+++ Formula/gnome-2048.rb
@@ -7,6 +7,7 @@ class Gnome2048 < Formula
   license "GPL-3.0-or-later"
 
   depends_on "desktop-file-utils" => :build
+  depends_on "itstool" => :build
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkgconf" => :build
