29abed6be5cb1dcfcf850ea44a7a07d7140138c0
Marcel <jsoservice@yahoo.com>
2026-01-19 06:44:55 -0500

    Update gnome-2048.rb

--- Formula/gnome-2048.rb
+++ Formula/gnome-2048.rb
@@ -11,6 +11,7 @@ class Gnome2048 < Formula
   depends_on "ninja" => :build
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
+  depends_on "valac" => :build
   depends_on "glib"
   depends_on "gtk4"
   depends_on "libadwaita"
