5b17fa6f1d1ddde51250831de256701de11d027b
Marcel <jsoservice@yahoo.com>
2026-01-19 22:30:21 -0500

    Update larswm.rb

--- Formula/larswm.rb
+++ Formula/larswm.rb
@@ -6,7 +6,7 @@ class Larswm < Formula
   sha256 "6c3dc08d84cf43cefefb90a884d3dde9a546d71e3d994286f5efd23fc7dca253"
   license "MIT"
 
-  depends_on "imake" => :build
+  depends_on "imake"
   depends_on "libx11"
 
   def install
