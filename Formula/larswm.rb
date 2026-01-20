dc470f5309498d4c43d7ddce191c8da840bc8952
Marcel <jsoservice@yahoo.com>
2026-01-19 22:19:06 -0500

    Add files via upload

---
+++ Formula/larswm.rb
@@ -0,0 +1,21 @@
+class Larswm < Formula
+  version "0.22pl3"
+  desc "A rewrite of 9wm that adds automatic tiling, virtual desktops and many more features"
+  homepage "http://porneia.free.fr/larswm/larswm.html"
+  url "https://ftp.lysator.liu.se/pub/X11/wm/amiwm/amiwm#{version}.tar.gz"
+  sha256 "6c3dc08d84cf43cefefb90a884d3dde9a546d71e3d994286f5efd23fc7dca253"
+  license "MIT"
+
+  depends_on "imake" => :build
+  depends_on "libx11"
+
+  def install
+    system "xmkmf"
+    system "make", "DESTDIR=#{prefix}"
+    system "make", "install", "DESTDIR=#{prefix}"
+  end
+  
+  test do
+    system "false"
+  end
+end
