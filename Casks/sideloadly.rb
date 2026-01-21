207bc61c57083b09635bbabba5e5938ac1e75496
Marcel <jsoservice@yahoo.com>
2026-01-20 22:21:28 -0500

    Add files via upload

---
+++ Casks/sideloadly.rb
@@ -0,0 +1,10 @@
+cask "Sideloadly" do
+  version "0.60.0"
+  sha256 "428d062af1ca819712fb12cb0ace25fa49c80d9735c73cda3cbaf09ffcd63212"
+  url "https://sideloadly.io/SideloadlySetup.dmg"
+  name "Sideloadly"
+  desc "Sideload your favorite games and apps on iOS, Apple Silicon Macs, and Apple TV without jailbreak"
+  homepage "https://sideloadly.io"
+
+  app "Sideloadly.app"
+end
