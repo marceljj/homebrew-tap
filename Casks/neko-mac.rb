bc5cf1b895aeeebfc3ceee8f513085685e1c5767
Marcel <jsoservice@yahoo.com>
2026-01-21 20:32:28 -0500

    Update and rename neko.rb to neko-mac.rb

--- Casks/neko-mac.rb
+++ Casks/neko-mac.rb
@@ -1,4 +1,4 @@
-cask "neko" do
+cask "neko-mac" do
   version "1.1"
   sha256 "ed3a93bb988754bc7a5f6af49e5af0b935d6f80190fa2ab1148d7abb9a5f1fd6"
   url "https://github.com/mdonoughe/neko-mac/releases/download/v#{version}/Neko-#{version}.dmg"
