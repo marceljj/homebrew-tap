268ca360685a35f51749c10306c63166bf7de06b
Marcel <jsoservice@yahoo.com>
2026-01-21 20:21:09 -0500

    Add files via upload

---
+++ Casks/neko-mac.rb
@@ -0,0 +1,11 @@
+cask "neko-mac" do
+  version "1.1"
+  sha256 "7fea2eb9de6a4694e56da9f7750f8f002d272891908b730020122b22beb59c0c"
+
+  url "https://github.com/mdonoughe/neko-mac/releases/download/v#{version}/Neko-#{version}.dmg"
+  name "Neko"
+  desc "Oneko in Cocoa"
+  homepage "hhttps://github.com/mdonoughe/neko-mac"
+
+  app "猫.app" => "Neko.app"
+end
