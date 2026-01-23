29f6b21126a73887db3a7e7652423d274c418529
Marcel <jsoservice@yahoo.com>
2026-01-22 21:00:53 -0500

    Add files via upload

---
+++ Formula/sent.rb
@@ -0,0 +1,26 @@
+class Sent < Formula
+  version "1"
+  desc "Simple plaintext presentation tool"
+  homepage "https://tools.suckless.org/sent"
+  url "https://dl.suckless.org/tools/sent-#{version}.tar.gz"
+  sha256 "7bf3de9311ce291ff36adf5315b78fa72373e3ab80ca71387fb5884bcbd7be33"
+  license "ISC"
+
+  depends_on "farbfeld"
+  depends_on "libx11"
+  
+  def install
+    inreplace "config.mk" do |s|
+      s.gsub! "PREFIX = /usr/local", "PREFIX = #{PREFIX}"
+      s.gsub! "-I${X11INC}", "-I#{incdir}"
+      s.gsub! "-L${X11LIB}", "-L#{libdir}"
+    end
+    
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "false"
+  end
+end
