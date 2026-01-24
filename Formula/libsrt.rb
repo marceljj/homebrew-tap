ed976e07b56aecf7d71316d71bc0c547640b0c34
Marcel <jsoservice@yahoo.com>
2026-01-24 01:41:41 -0500

    Add files via upload

---
+++ Formula/libsrt.rb
@@ -0,0 +1,20 @@
+class Libsrt < Formula
+  version "git-2021.09"
+  desc "A C library for writing fast and safe C code, faster"
+  homepage "https://github.com/faragon/libsrt"
+  url "https://github.com/faragon/libsrt.git", branch: "master"
+  license "BSD-3-Clause"
+  
+  depends_on "autoconf" => :build
+  
+  def install
+    system "autoreconf", "-fi"
+    system "./configure", *std_configure_args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "make", "check"
+  end
+end
