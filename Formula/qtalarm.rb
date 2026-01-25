424d34d4b4bdd3b5bf664d4d1a747afb6c8fb04b
Marcel <jsoservice@yahoo.com>
2026-01-24 22:52:00 -0500

    Update qtalarm.rb

--- Formula/qtalarm.rb
+++ Formula/qtalarm.rb
@@ -3,16 +3,16 @@ class Qtalarm< Formula
   desc "A handy alarm clock Program written in QT"
   homepage "https://github.com/CountMurphy/QTalarm"
   url "https://github.com/CountMurphy/QTalarm/archive/refs/tags/#{version}.tar.gz"
-  sha256 "123"
+  sha256 "c259f8665336fd6b418a9aeaf98618cff3c508cbcdb2d466e4dbd598c338ef59"
   license "GPL-3.0"
 
   depends_on "qtbase" => :build
   depends_on "qt@5"
   
   def install
-    bin.install "fet.sh"
-    doc.install "LICENSE"
-    doc.install "README.md"
+    system "qmake"
+    system "make"
+    
   end
 
   test do
