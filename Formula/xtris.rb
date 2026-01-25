3bae7e146dc4b268b5d8f74d15770039f247d2b1
Marcel <jsoservice@yahoo.com>
2026-01-25 01:39:05 -0500

    Add files via upload

---
+++ Formula/xtris.rb
@@ -0,0 +1,25 @@
+class Xtris < Formula
+  version "1.15"
+  desc "A multi-player version of Tetris for the X Window system"
+  homepage "https://ftp.netbsd.org/pub/pkgsrc/distfiles/xtris-#{version}.tar.gz"
+  url "https://ftp.netbsd.org/pub/pkgsrc/distfiles/xtris-#{version}.tar.gz"
+  sha256 "5791b30795382a18536201c95c686f62ade6975fc4e63c651bff36226c933d68"
+  license "GPL-2.0"
+
+  depends_on "libx11"
+  
+  def install
+    inreplace "Makefile" do |s|
+      s.gsub! "BINDIR = /usr/local/bin", "BINDIR = #{prefix}/bin"
+      s.gsub! "MANDIR = /usr/local/man", "BINDIR = #{prefix}/share/man"
+      s.gsub! "XLIBS = -lX11", "XLIBS = -lX11 -L#{Formula["libx11"].opt_lib} -I#{Formula["libx11"].opt_include}"
+    end
+    
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "false"
+  end
+end
