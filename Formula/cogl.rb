c866e6ee88f67dae43d1338104d02626aade4314
Marcel <jsoservice@yahoo.com>
2026-01-24 20:38:16 -0500

    Add files via upload

---
+++ Formula/cogl.rb
@@ -0,0 +1,36 @@
+class Cogl < Formula
+  version "1.22.8"
+  desc "A small open source library for using 3D graphics hardware for rendering"
+  homepage "https://gitlab.gnome.org/Archive/cogl"
+  url "https://gitlab.gnome.org/Archive/cogl/-/archive/#{version}/cogl-#{version}.tar.gz"
+  sha256 "bf36baf14e6aa0388e4c4addf8c35a154f1c2252986dbae06e5c6f6187b531c4"
+  license "all_of: ["BSD-3-Clause", "MIT", "SGI-B-2.0"]"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "pkgconf" => :build
+  depends_on "cairo"
+  depends_on "gdk-pixbuf"
+  depends_on "glib"
+  depends_on "gobject-introspection"
+  depends_on "gstreamer"
+  depends_on "gtk-doc"
+  depends_on "libx11"
+  depends_on "libxcomposite"
+  depends_on "libxdamage"
+  depends_on "libxext"
+  depends_on "libxfixes"
+  depends_on "mesa"
+  depends_on "pango"
+
+  def install
+    system "./autogen.sh"
+    system "./configure", *std_configure_args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "false"
+  end
+end
