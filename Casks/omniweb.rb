e5f0f56f8ae38e3ca9646344cbe2e055b2dd3111
Marcel <jsoservice@yahoo.com>
2026-01-26 00:50:19 -0500

    Add files via upload

---
+++ Casks/omniweb.rb
@@ -0,0 +1,10 @@
+cask "omniweb" do
+  version "636.0.8"
+  sha256 "dff70db289f3b2cd93a5cf4da943cd4a2cbb58fd485d99ede08bedf093ece29e"
+  url "https://omnistaging.omnigroup.com/omniweb/releases/OmniWeb-v#{version}-a8b3eaf9d0-Test.dmg"
+  name "Gophie"
+  desc "A powerful, award-winning, feature-rich alternative to mainstream web browsers"
+  homepage "https://omnistaging.omnigroup.com/omniweb"
+
+  app "OmniWeb.app"
+end
