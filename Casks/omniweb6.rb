74f6b4e4c849ad2f4872046572dfb9dd5f075cdc
Marcel <jsoservice@yahoo.com>
2026-01-26 01:01:46 -0500

    Update omniweb6.rb

--- Casks/omniweb6.rb
+++ Casks/omniweb6.rb
@@ -2,7 +2,7 @@ cask "omniweb6" do
   version "636.0.8"
   sha256 "dff70db289f3b2cd93a5cf4da943cd4a2cbb58fd485d99ede08bedf093ece29e"
   url "https://omnistaging.omnigroup.com/omniweb/releases/OmniWeb-v#{version}-a8b3eaf9d0-Test.dmg"
-  name "Gophie"
+  name "OmniWeb 6"
   desc "A powerful, award-winning, feature-rich alternative to mainstream web browsers"
   homepage "https://omnistaging.omnigroup.com/omniweb"
 
