268e8e0182796d2d7d6c403982234610bf26ef79
Marcel <jsoservice@yahoo.com>
2026-01-26 22:10:45 -0500

    Add files via upload

---
+++ Formula/hunspell-colorize.rb
@@ -0,0 +1,21 @@
+class HunspellColorize < Formula
+  version "git-2026.01"
+  desc "Wrapper around 'less' to colorize spelling mistakes using Hunspell"
+  homepage "https://github.com/torvalds/HunspellColorize"
+  url "https://github.com/torvalds/HunspellColorize.git", branch: "main"
+  license "GPL-2.0"
+
+  depends_on "pkgconf" => :build
+  depends_on "hunspell"
+  
+  def install
+    inreplace "Makefile", "$(HOME)/bin", "#{bin}"
+  end
+  
+  system "make"
+  system "make", "install"
+
+  test do
+    system "false"
+  end
+end
