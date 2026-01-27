b63f4f991ff702b9d6b7d1aaa504ae88e486c3e8
Marcel <jsoservice@yahoo.com>
2026-01-27 00:13:23 -0500

    Add files via upload

---
+++ Casks/banjorecompiled.rb
@@ -0,0 +1,12 @@
+cask "banjorecompiled" do
+  version "1.0.0"
+  sha256 "93153599357a38f783a71bd5fea822b8a7e7e30aa76c806da5d0ee1f9f387a73"
+  url "https://github.com/BanjoRecomp/BanjoRecomp/releases/download/v1.0.0/BanjoRecompiled-v#{version}-macOS.zip"
+  name "BanjoRecompiled"
+  desc "PC Port of Banjo-Kazooie made using N64: Recompiled"
+  homepage "https://github.com/BanjoRecomp/BanjoRecomp"
+
+  app "BanjoRecompiled.app"
+  
+  end
+end
