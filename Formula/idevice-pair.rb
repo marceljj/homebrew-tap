c3a9c307aae7ae94fdb6b503a3f2f1572a58af33
Marcel <jsoservice@yahoo.com>
2026-01-28 03:52:35 -0500

    Update idevice-pair.rb

--- Formula/idevice-pair.rb
+++ Formula/idevice-pair.rb
@@ -3,7 +3,7 @@ class IdevicePair < Formula
   desc "Generate pair records for iOS and save them"
   homepage "https://github.com/jkcoxson/idevice_pair"
   url "https://github.com/jkcoxson/idevice_pair/archive/refs/tags/v#{version}.tar.gz"
-  sha256 "e1ab480fb2c608b6a15a45afc0b9acf96435a786e254d390dc21f359ce15d711"
+  sha256 "195cdf53a54145a2276617a6775382f652d419040b19925ee9a43116933d3dc9"
   license "MIT"
 
   depends_on "rust" => :build
@@ -11,7 +11,6 @@ class IdevicePair < Formula
 
   def install
     system "cargo", "build", "--locked", "--release"
-    
     bin.install "target/release/idevice_pair"
     doc.install "README.md"
   end
