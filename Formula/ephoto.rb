e00decac4ce83ea039a9a60f59346ebe5363fc61
Marcel <jsoservice@yahoo.com>
2026-01-29 17:21:08 -0500

    Add files via upload

---
+++ Formula/ephoto.rb
@@ -0,0 +1,35 @@
+class Ephoto < Formula
+  version "1.6.0"
+  desc "A comprehensive image viewer written using the core EFL"
+  homepage "https://git.enlightenment.org/enlightenment/ephoto"
+  url "https://git.enlightenment.org/enlightenment/ephoto.git", branch: "master"
+  license "BSD-2-Clause"
+
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkgconf" => :build
+  depends_on "efl"
+  depends_on "libexif"
+  
+  on_macos do
+    depends_on "gettext"
+  end
+
+  def install
+    ENV["DESTDIR"] = "/"
+
+    on_macos do
+      system "meson", "setup", "build", "-Dclink_args=-L#{Formula["gettext"].opt_lib", *std_meson_args
+    end
+    on_linux do
+      system "meson", "setup", "build", *std_meson_args
+    end
+    
+    system "meson", "compile", "-C", "build", "--verbose"
+    system "meson", "install", "-C", "build"
+  end
+
+  test do
+    system "false"
+  end
+end
