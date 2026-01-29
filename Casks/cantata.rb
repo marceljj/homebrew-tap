8edbc8cb8c1e5a2313e1594ef98733116d1c74a8
Marcel <jsoservice@yahoo.com>
2026-01-28 21:39:08 -0500

    Add files via upload

---
+++ Casks/cantata.rb
@@ -0,0 +1,13 @@
+cask "cantata" do
+  version "3.4.0"
+  sha256 "b77c06ebbc8fbb9b5a879752a89596153a99ff61acddc384c97f58fc42f7aea5"
+
+  url "https://github.com/nullobsi/cantata/releases/download/v#{version}/Cantata-v#{version}-Universal-Darwin.dmg"
+  name "Cantata"
+  desc "Qt Graphical MPD Client"
+  homepage "https://github.com/nullobsi/cantata"
+
+  app "Cantata.app"
+  binary "#{appdir}/Cantata.app/Contents/MacOS/Cantata" => "cantata"
+  binary "#{appdir}/Cantata.app/Resources/scripts/cantata-dynamic"
+end
