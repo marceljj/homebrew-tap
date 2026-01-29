4edce8d6ca278dfb3fd3c23deb9678c13a0c186f
Marcel <jsoservice@yahoo.com>
2026-01-28 22:04:28 -0500

    Update tidal-dl.rb

--- Formula/tidal-dl.rb
+++ Formula/tidal-dl.rb
@@ -5,7 +5,7 @@ class TidalDl < Formula
   desc "An application that lets you download videos and tracks from Tidal"
   homepage "http://doc.yaronzz.com/post/tidal_dl_installation"
   url "https://github.com/yaronzz/Tidal-Media-Downloader/archive/refs/tags/#{version}.tar.gz"
-  sha256 "abc123abc123abc123abc123abc123abc123abc123abc123abc123abc123abc1"
+  sha256 "f74722b8f70e720abc74eb68d79deec13803b7506504feeb7e564aa6a74d3491"
   license "Apache-2.0"
 
   depends_on "ffmpeg"
