a36d7ed9b69e61a1b81cce534be125ae69385716
Marcel <jsoservice@yahoo.com>
2026-01-30 00:31:38 -0500

    Update dsdmine.rb

--- Casks/d/dsdmine.rb
+++ Casks/d/dsdmine.rb
@@ -1,6 +1,6 @@
 cask "dsdmine" do
   version "2.1"
-  sha256 "b77c06ebbc8fbb9b5a879752a89596153a99ff61acddc384c97f58fc42f7aea5"
+  sha256 "ed09b29b8c478d7c23eb5f1c266cf1819b465edfe38104dff9a85aab2c6d089c"
 
   url "https://github.com/DragonSWDev/dsdmine/releases/download/v2.1/dsdmine-macos-arm64-2.1.zip"
   name "dsdmine"
