d29f74560ad3f33110bfa75e81d33065d0972c6d
Marcel <jsoservice@yahoo.com>
2026-01-30 01:14:06 -0500

    Add files via upload

---
+++ Formula/e/equate.rb
@@ -0,0 +1,25 @@
+class Equate < Formula
+  version "2017.08"
+  desc "A calculator based on EFL"
+  homepage "https://git.enlightenment.org/vtorri/equate"
+  url "https://git.enlightenment.org/vtorri/equate.git", branch: "master"
+  license "MIT"
+
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkgconf" => :build
+  depends_on "efl"
+  end
+
+  def install
+    ENV["DESTDIR"] = "/"
+
+    system "meson", "setup", "build", *std_meson_args
+    system "meson", "compile", "-C", "build", "--verbose"
+    system "meson", "install", "-C", "build"
+  end
+
+  test do
+    system "false"
+  end
+end
