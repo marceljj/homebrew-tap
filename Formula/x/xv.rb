a2b933cae7f2ae187fccd2cdb338ec72775879df
Marcel <jsoservice@yahoo.com>
2026-02-01 19:26:14 -0500

    Add files via upload

---
+++ Formula/x/xv.rb
@@ -0,0 +1,28 @@
+class Xv < Formula
+  version "6.1.0"
+  desc "An interactive image manipulation program for the X Window System"
+  homepage "https://xv.trilon.com"
+  url "https://github.com/jasper-software/xv/releases/download/v#{version}/xv-3.10a-js-#{version}.tar.gz"
+  sha256 "bfd0df563a7d5e6632fd22a4789d6a2e5ec24c83027d058a563eebc6dc74ca9c"
+  license :cannot_represent
+
+  depends_on "cmake" => :build
+  depends_on "ninja" => :build
+  depends_on "jasper"
+  depends_on "jpeg-turbo"
+  depends_on "libpng"
+  depends_on "libtiff"
+  depends_on "libwebp"
+  depends_on "libx11"
+  depends_on "libxrandr"
+  
+  def install
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    system "false"
+  end
+end
