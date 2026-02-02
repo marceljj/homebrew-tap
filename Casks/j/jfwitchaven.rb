d7191541e0815c5fd776f4037e3e549b0e81d45b
Marcel <jsoservice@yahoo.com>
2026-02-01 21:57:01 -0500

    Update sha256 checksum for jfwitchaven

--- Casks/j/jfwitchaven.rb
+++ Casks/j/jfwitchaven.rb
@@ -1,6 +1,6 @@
 cask "jfwitchaven" do
   version "20251227"
-  sha256 "7eafc5efe620345260d06354e09e951deda2201ced7a0a42e5a8090632200196"
+  sha256 "f118b875b7793e5d0fab54b28291785eb71ffdeb6b8282426f289169d4c72657"
   url "https://www.jonof.id.au/files/jfwhaven/jfwhaven-#{version}-mac.zip"
   name "JFWitchaven"
   desc "Source port of Witchaven"
