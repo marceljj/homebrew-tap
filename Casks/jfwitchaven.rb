e6166588a78676dd7651b6255489f829dd68f3d1
Marcel <jsoservice@yahoo.com>
2026-02-01 21:55:24 -0500

    Add files via upload

---
+++ Casks/jfwitchaven.rb
@@ -0,0 +1,10 @@
+cask "jfwitchaven" do
+  version "20251227"
+  sha256 "7eafc5efe620345260d06354e09e951deda2201ced7a0a42e5a8090632200196"
+  url "https://www.jonof.id.au/files/jfwhaven/jfwhaven-#{version}-mac.zip"
+  name "JFWitchaven"
+  desc "Source port of Witchaven"
+  homepage "https://www.jonof.id.au/jfwitchaven"
+
+  app "JFWitchaven.app"
+end
