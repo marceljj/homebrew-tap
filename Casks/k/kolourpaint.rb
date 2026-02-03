7c2e3c28b069f2404f6bb1e449ee3ed1379291d1
Marcel <jsoservice@yahoo.com>
2026-02-03 01:58:48 -0500

    Fix URL formatting for KolourPaint Cask

--- Casks/k/kolourpaint.rb
+++ Casks/k/kolourpaint.rb
@@ -3,7 +3,7 @@ cask "kolourpaint" do
   version "1236"
   sha256 arm:   "aa71b70f271cd1c5d62e34cf195fdfbcb230639421b45385b7560fbf0e3f6afc",
          intel: "e6eef254d0832f34111e90a42e451af903ff77ab1e395848bb437dd486d0cf0b"
-  url "https://cdn.kde.org/ci-builds/graphics/kolourpaint/master/macos-arm64/kolourpaint-master-#{version}-macos-clang-#{arch}.dmg",
+  url "https://cdn.kde.org/ci-builds/graphics/kolourpaint/master/macos-arm64/kolourpaint-master-#{version}-macos-clang-#{arch}.dmg"
   name "KolourPaint"
   desc "A simple painting program to quickly create raster images"
   homepage "https://apps.kde.org/kolourpaint"
