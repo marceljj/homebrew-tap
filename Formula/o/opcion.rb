30ccfea8af1c9b22c44d97ded4026a614f46a4c3
Marcel <jsoservice@yahoo.com>
2026-02-04 03:17:17 -0500

    Update opcion.rb

--- Formula/o/opcion.rb
+++ Formula/o/opcion.rb
@@ -4,7 +4,7 @@ class Opcion < Formula
   homepage "https://opcion.sourceforge.net"
   url "https://downloads.sourceforge.net/project/opcion/Opcion%20Font%20Viewer/Opcion%20Font%20Viewer%20v1.1.1/Opcion_v#{version}.jar"
   sha256 "b28dd5a013aebf923585d8c7c09468c48f503ed594622ebce8babce00cd54bc4"
-  license :public_domain
+  license "GPL-2.0"
   
   depends_on "openjdk"
 
