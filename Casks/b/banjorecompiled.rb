3cb71c5804ad00b854d9b205ba3325de50a6afde
Marcel <jsoservice@yahoo.com>
2026-02-04 03:21:04 -0500

    Update banjorecompiled.rb

--- Casks/b/banjorecompiled.rb
+++ Casks/b/banjorecompiled.rb
@@ -1,6 +1,6 @@
 cask "banjorecompiled" do
-  version "1.0.0"
-  sha256 "93153599357a38f783a71bd5fea822b8a7e7e30aa76c806da5d0ee1f9f387a73"
+  version "1.0.1"
+  sha256 "adc1cd5c86262b3cc932bde8ad33abf1e1efe3d10872670cb0fbe53113cce7da"
   url "https://github.com/BanjoRecomp/BanjoRecomp/releases/download/v1.0.0/BanjoRecompiled-v#{version}-macOS.zip"
   name "BanjoRecompiled"
   desc "PC Port of Banjo-Kazooie made using N64: Recompiled"
