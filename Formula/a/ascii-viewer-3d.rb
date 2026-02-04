0e2d02db02b7baba15d3f7ab927994eea8b4a542
Marcel <jsoservice@yahoo.com>
2026-02-04 01:11:30 -0500

    Update homepage URL for AsciiViewer3d

--- Formula/a/ascii-viewer-3d.rb
+++ Formula/a/ascii-viewer-3d.rb
@@ -1,7 +1,7 @@
 class AsciiViewer3d < Formula
   version "1.4.0"
   desc "Viewer of 3D models in ASCII without graphic libraries"
-  homepage "https://audacious-media-player.org"
+  homepage "https://github.com/autopawn/3d-ascii-viewer"
   url "https://github.com/autopawn/3d-ascii-viewer/archive/refs/tags/v#{version}.tar.gz"
   sha256 "c390cbd4c80f6a7c5bdd9cab02174aaa8d63c0eeb18134deb8ed85463395d81c"
   license "MIT"
