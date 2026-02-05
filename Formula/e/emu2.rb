e3c50d9f9b6b247f885cc1ef3861581498751603
Marcel <jsoservice@yahoo.com>
2026-02-05 02:14:00 -0500

    Update emu2.rb

--- Formula/e/emu2.rb
+++ Formula/e/emu2.rb
@@ -1,5 +1,5 @@
 class Emu2 < Formula
-  version "git-2025.11"
+  version "git-2026.01"
   desc "Simple x86 and DOS emulator for the UNIX terminal"
   homepage "https://github.com/dmsc/emu2"
   url "https://github.com/dmsc/emu2.git", branch: "master"
