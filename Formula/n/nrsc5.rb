af6fabe19aa73902846f48783fa89dd2eb94325e
Marcel <jsoservice@yahoo.com>
2026-02-05 23:09:50 -0500

    Update nrsc5.rb

--- Formula/n/nrsc5.rb
+++ Formula/n/nrsc5.rb
@@ -1,7 +1,7 @@
 class Nrsc5 < Formula
   desc "NRSC-5 receiver for rtl-sdr"
   version "3.1.0"
-  homepage "http://theori.io/research/nrsc-5-c"
+  homepage "https://github.com/theori-io/nrsc5"
   url "https://github.com/theori-io/nrsc5/archive/refs/tags/v#{version}.tar.gz"
   sha256 "66a1d199e92fd7b9464d55ece633757cb9d1daf37d4d80e0f4f8f63da089295e"
   license "GPL-3.0-or-later"
