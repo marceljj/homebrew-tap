5b914ef9124914aa27f328d38f29b6c2161d4efc
Marcel <jsoservice@yahoo.com>
2026-02-06 02:45:13 -0500

    Update nrsc5.rb

--- Formula/n/nrsc5.rb
+++ Formula/n/nrsc5.rb
@@ -6,8 +6,8 @@ class Nrsc5 < Formula
   sha256 "66a1d199e92fd7b9464d55ece633757cb9d1daf37d4d80e0f4f8f63da089295e"
   license "GPL-3.0-or-later"
 
-  depends_on "git" => :build
   depends_on "cmake" => :build
+  depends_on "git" => :build
   depends_on "doxygen" => :build
   depends_on "pkgconf" => :build
   depends_on "fftw"
