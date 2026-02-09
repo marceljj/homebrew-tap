fc2e5fc9eee6a108ece2946e6aa11c63351a5847
Marcel <jsoservice@yahoo.com>
2026-02-09 17:20:22 -0500

    Add files via upload

---
+++ Formula/t/tvision.rb
@@ -0,0 +1,24 @@
+class Tvision < Formula
+  desc "A modern port of Turbo Vision 2.0"
+  version "git-2026.01"
+  homepage "https://github.com/magiblot/tvision"
+  url "https://github.com/magiblot/tvision.git", branch: "master"
+  license "MIT"
+
+  depends_on "cmake" => :build
+  
+  on_linux do
+    depends_on "ncurses"
+    depends_on "xclip"
+  end
+  
+  def install
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    system "false"
+  end
+end
