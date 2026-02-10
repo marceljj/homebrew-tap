4d7df835f47a0b16fb13b09a4273974d3466c51a
Marcel <jsoservice@yahoo.com>
2026-02-09 23:58:34 -0500

    Add files via upload

---
+++ Formula/h/hygg.rb
@@ -0,0 +1,20 @@
+class Hygg < Formula
+  version "0.1.19"
+  desc "Minimalistic Vim-like TUI document reader"
+  homepage "https://github.com/MightyBurger/curveball"
+  url "https://github.com/kruseio/hygg/archive/refs/tags/#{version}.tar.gz"
+  sha256 "36491747e9d47cf7d24ae0666fd2908535eac437e8223a6bd88465a90e1cf35a"
+  license "AGPL-3.0"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "build", *std_cargo_args
+    bin.install "target/release/hygg"
+    doc.install Dir["docs/*"]
+  end
+
+  test do
+    system "false"
+  end
+end
