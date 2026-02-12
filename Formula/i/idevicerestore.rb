6a65054500610f7df41776061065286f6b8fa196
Marcel <jsoservice@yahoo.com>
2026-02-12 00:51:08 -0500

    Update idevicerestore.rb

--- Formula/i/idevicerestore.rb
+++ Formula/i/idevicerestore.rb
@@ -1,9 +1,8 @@
 class Idevicerestore < Formula
-  version "1.0.0"
+  version "git-2025.11"
   desc "Generate pair records for iOS and save them"
   homepage "https://github.com/libimobiledevice/idevicerestore"
-  url "https://github.com/libimobiledevice/idevicerestore/releases/download/1.0.0/idevicerestore-#{version}.tar.bz2"
-  sha256 "32712e86315397fd2e8999e77a2d2f790c67f6b4aa50d4d1c64cb2c4609836f7"
+  url "https://github.com/libimobiledevice/idevicerestore.git", branch: "master"
   license "LGPL-3.0"
 
   depends_on "autoconf" => :build
