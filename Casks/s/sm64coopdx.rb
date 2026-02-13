0e4112096afe424f7be145862ef3b74de48b8712
Marcel <jsoservice@yahoo.com>
2026-02-12 23:22:42 -0500

    Update sm64coopdx.rb

--- Casks/s/sm64coopdx.rb
+++ Casks/s/sm64coopdx.rb
@@ -1,8 +1,6 @@
 cask "sm64coopdx" do
   arch arm: "ARM", intel: "Intel"
   version "1.4.1"
-  sha256 arm: "5ae446eb069d903b770ab735db0c60f37cfecca59e4b66a68fc05a5587a2ec6d",
-         intel: "b8bad2ddc1f68908010941bc578ffb3db3964a6fbe9e62c8fae54a177f6eb499"
   url "https://github.com/coop-deluxe/sm64coopdx/releases/download/v#{version}/sm64coopdx_macOS_#{arch}.zip"
   name "Super Mario 64 Coop Deluxe"
   desc "An online multiplayer project for the Super Mario 64 PC port "
