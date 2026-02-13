6eaa0644267f6caca7d4ac7f51bf552a4620830a
Marcel <jsoservice@yahoo.com>
2026-02-12 23:26:55 -0500

    Update infekt.rb

--- Casks/i/infekt.rb
+++ Casks/i/infekt.rb
@@ -1,6 +1,8 @@
 cask "infekt" do
   arch arm: "aarch64", intel: "intel"
   version "2.0.0-alpha1"
+  sha256 arm: "06cb4cdaacd5e0784087be3dd6def5552282fdb36d5928639addb535b568c9d0",
+         intel: "c0867a52374843cbb34a67fd632881ad60b758262d66be5abb0e86b046c90fee"
   url "https://github.com/syndicodefront/infekt/releases/download/v#{version}/iNFekt-v#{version}-macos-#{arch}.dmg"
   name "iNFekt NFO Viewer"
   desc "The ultimate best NFO viewer, ever"
