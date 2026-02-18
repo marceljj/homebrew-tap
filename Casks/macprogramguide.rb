d1a20254add8081a592e5a060eb8f7b610220e5d
Marcel <jsoservice@yahoo.com>
2026-02-17 20:47:32 -0500

    Add files via upload

---
+++ Casks/macprogramguide.rb
@@ -0,0 +1,10 @@
+cask "macprogramguide" do
+  version "1.4.4"
+  sha256 "7eafc5efe620345260d06354e09e951deda2201ced7a0a42e5a8090632200196"
+  url "https://coolmacsoftware.com/bin/MacProgramGuide#{version}.zip"
+  name "MacProgramGuide"
+  desc "Frontend for SchedulesDirect"
+  homepage "https://coolmacsoftware.com/mpgmain.html"
+
+  app "MacProgramGuide"
+end
