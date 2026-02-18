0c75223a55bcb4f1c8734ba3aab14eb41e95d50f
Marcel <jsoservice@yahoo.com>
2026-02-17 20:48:48 -0500

    Update macprogramguide.rb

--- Casks/m/macprogramguide.rb
+++ Casks/m/macprogramguide.rb
@@ -1,6 +1,6 @@
 cask "macprogramguide" do
   version "1.4.4"
-  sha256 "7eafc5efe620345260d06354e09e951deda2201ced7a0a42e5a8090632200196"
+  sha256 "1ed2fefdf1f2006be0d5ff716138fdddee4c85a89d3f1e16cf0e6b389c8b3be3"
   url "https://coolmacsoftware.com/bin/MacProgramGuide#{version}.zip"
   name "MacProgramGuide"
   desc "Frontend for SchedulesDirect"
