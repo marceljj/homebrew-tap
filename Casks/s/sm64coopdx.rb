07190982a8241a0234f183685329e37fa7b7d32e
Marcel <jsoservice@yahoo.com>
2026-02-21 22:39:29 -0500

    Update sm64coopdx.rb

--- Casks/s/sm64coopdx.rb
+++ Casks/s/sm64coopdx.rb
@@ -5,7 +5,7 @@ cask "sm64coopdx" do
          intel: "b8bad2ddc1f68908010941bc578ffb3db3964a6fbe9e62c8fae54a177f6eb499"
   url "https://github.com/coop-deluxe/sm64coopdx/releases/download/v#{version}/sm64coopdx_macOS_#{arch}.zip"
   name "Super Mario 64 Coop Deluxe"
-  desc "An online multiplayer project for the Super Mario 64 PC port "
+  desc "An online multiplayer project for the Super Mario 64 PC port"
   homepage "https://sm64coopdx.com"
 
   app "sm64coopdx.app"
