9b2134285f09dce13ee5edff8da12a6acb43bf86
Marcel <jsoservice@yahoo.com>
2026-03-07 21:10:09 -0500

    Add files via upload

---
+++ Casks/s/sshfs.rb
@@ -0,0 +1,13 @@
+cask "sshfs" do
+  version "3.7.5"
+  sha256 "611713612179cf7ccd2995051165da7d19e0ca199ae70d9680c3d3551f456d46"
+  url "https://github.com/libfuse/sshfs/releases/download/sshfs-#{version}/sshfs-#{version}.pkg"
+      verified: "github.com/libfuse/sshfs"
+  name "sshfs"
+  desc "A network filesystem client to connect to SSH servers"
+  homepage "https://github.com/libfuse/sshfs"
+
+  pkg "sshfs-#{version}.pkg"
+
+  uninstall pkgutil: "io.macfuse.installer.components.sshfs"
+end
