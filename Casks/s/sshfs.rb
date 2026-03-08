8d50ec003d92990ff0ce8e43cd67db59a92dada1
Marcel <jsoservice@yahoo.com>
2026-03-07 21:11:38 -0500

    Update sshfs.rb

--- Casks/s/sshfs.rb
+++ Casks/s/sshfs.rb
@@ -1,7 +1,7 @@
 cask "sshfs" do
   version "3.7.5"
   sha256 "611713612179cf7ccd2995051165da7d19e0ca199ae70d9680c3d3551f456d46"
-  url "https://github.com/libfuse/sshfs/releases/download/sshfs-#{version}/sshfs-#{version}.pkg"
+  url "https://github.com/libfuse/sshfs/releases/download/sshfs-#{version}/sshfs-#{version}.pkg",
       verified: "github.com/libfuse/sshfs"
   name "sshfs"
   desc "A network filesystem client to connect to SSH servers"
