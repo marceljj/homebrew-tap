e7c765afe4c49883e0a836b9b22250bf7facafb5
Marcel <jsoservice@yahoo.com>
2026-03-07 21:15:26 -0500

    Update and rename sshfs3-mac.rb to sshfs3.rb

--- Casks/s/sshfs3.rb
+++ Casks/s/sshfs3.rb
@@ -1,4 +1,4 @@
-cask "sshfs3-mac" do
+cask "sshfs3" do
   version "3.7.5"
   sha256 "611713612179cf7ccd2995051165da7d19e0ca199ae70d9680c3d3551f456d46"
   url "https://github.com/libfuse/sshfs/releases/download/sshfs-#{version}/sshfs-#{version}.pkg",
